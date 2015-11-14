c0c145c76f4447f6435abcf92353c99d5b8101a0
Bruce Zu <bruce.zu.run10@gmail.com>
2015-11-13 20:27:43 -0800

    Add SinglyLinkedList and its test class

---
+++ DataStructuresAndAlgorithmsInJava/src/main/java/charter3/SinglyLinkedList.java
@@ -0,0 +1,152 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package charter3;
+
+public class SinglyLinkedList<T extends Object> {
+    private class Node {
+        private T content;
+        private Node next;
+
+        private Node(T content, Node next) {
+            this.content = content;
+            this.next = next;
+        }
+    }
+
+    private Node head = null;
+    private int sizeOfList = 0;
+
+    private int indexOfEndNode() {
+        // index starts from 0
+        return sizeOfList - 1;
+    }
+
+    private int checkPositionIndex(int positionIndex) {
+        if (positionIndex < 0 || indexOfEndNode() < positionIndex) {
+            throw new IndexOutOfBoundsException("position index is wrong or this list is empty");
+        }
+        return positionIndex;
+    }
+
+    private Node getEndNode() {
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private Node getNodeOf(int index) {
+        checkPositionIndex(index);
+
+        Node current = head;
+        int i = 0;
+        while (i != index) {
+            current = current.next;
+            i++;
+        }
+        return current;
+    }
+
+    /*
+ Be Default, the Added one will be the head
+  */
+    public void add(T newContent) {
+        this.head = new Node(newContent, head);
+        sizeOfList++;
+    }
+
+    public void appendToTheEnd(T newContent) {
+        getEndNode().next = new Node(newContent, null);
+        sizeOfList++;
+    }
+
+    public void addBefore(T newContent, int index) {
+        checkPositionIndex(index);
+        if (index == 0) {
+            add(newContent);
+        }
+        addAfter(newContent, index - 1);
+    }
+
+    public void addAfter(T newContent, int index) {
+        checkPositionIndex(index);
+        if (index == indexOfEndNode()) {
+            appendToTheEnd(newContent);
+        }
+        Node n = getNodeOf(index);
+        n.next = new Node(newContent, n.next);
+        sizeOfList++;
+    }
+
+    public T deleteHead() {
+        checkPositionIndex(0);
+        T re = head.content;
+        head = head.next;
+        sizeOfList--;
+        return re;
+    }
+
+    public T deleteEnd() {
+        return delete(indexOfEndNode());
+    }
+
+    public T delete(int index) {
+        checkPositionIndex(index);
+        if (sizeOfList == 0) {
+            return deleteHead();
+        }
+        Node beforeIt = getNodeOf(index - 1);
+        Node it = beforeIt.next;
+
+        beforeIt.next = it.next;
+        sizeOfList--;
+
+        return it.content;
+    }
+
+    public T update(int index, T newContent) {
+        Node n = getNodeOf(index);
+        T re = n.content;
+        n.content = newContent;
+        return re;
+    }
+
+    public T updateHead(T newContent) {
+        T re = head.content;
+        head.content = newContent;
+        return re;
+    }
+
+    public T updateEnd(T newContent) {
+        Node end = getEndNode();
+        T re = end.content;
+        end.content = newContent;
+        return re;
+    }
+
+    public T getHead() {
+        return head.content;
+    }
+
+    public T getEnd() {
+        return getEndNode().content;
+    }
+
+    public T get(int index) {
+        checkPositionIndex(index);
+        return getNodeOf(index).content;
+    }
+}
---
+++ DataStructuresAndAlgorithmsInJava/src/test/java/SinglyLinkedListTest.java
@@ -0,0 +1,127 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import com.google.common.truth.Truth;
+import org.junit.Before;
+import org.junit.Test;
+import charter3.SinglyLinkedList;
+
+
+public class SinglyLinkedListTest {
+    private SinglyLinkedList list;
+
+    @Before
+    public void setup() {
+        list = new SinglyLinkedList();
+    }
+
+    @Test(timeout = 90000L, expected = IndexOutOfBoundsException.class)
+    public void testAdd() {
+        list.addAfter(3, 3);
+    }
+
+    @Test(timeout = 90000L, expected = IndexOutOfBoundsException.class)
+    public void testDelete() {
+        list.delete(0);
+    }
+
+    @Test(timeout = 90000L, expected = IndexOutOfBoundsException.class)
+    public void testUpdate() {
+        list.update(3, 3);
+    }
+
+    @Test(timeout = 90000L, expected = IndexOutOfBoundsException.class)
+    public void testGetOf() {
+        list.get(2);
+    }
+
+    @Test(timeout = 90000L, expected = Test.None.class)
+    public void testWithString() {
+        list.add("head");
+        Truth.assertThat(list.getHead()).isEqualTo("head");
+
+        list.appendToTheEnd("end");
+        Truth.assertThat(list.getEnd()).isEqualTo("end");
+
+        list.addAfter("second", 0);
+        Truth.assertThat(list.get(1)).isEqualTo("second");
+
+        list.addBefore("third", 2);
+        Truth.assertThat(list.get(2)).isEqualTo("third");
+
+        Truth.assertThat(list.delete(1)).isEqualTo("second");
+
+        Truth.assertThat(list.get(0)).isEqualTo("head");
+        Truth.assertThat(list.get(1)).isEqualTo("third");
+        Truth.assertThat(list.get(2)).isEqualTo("end");
+
+        Truth.assertThat(list.updateHead("zero")).isEqualTo("head");
+        Truth.assertThat(list.get(0)).isEqualTo("zero");
+        Truth.assertThat(list.updateEnd("last")).isEqualTo("end");
+        Truth.assertThat(list.get(2)).isEqualTo("last");
+
+        Truth.assertThat(list.deleteEnd()).isEqualTo("last");
+        Truth.assertThat(list.getEnd()).isEqualTo("third");
+
+        Truth.assertThat(list.deleteHead()).isEqualTo("zero");
+        Truth.assertThat(list.getEnd()).isEqualTo("third");
+        Truth.assertThat(list.getHead()).isEqualTo("third");
+
+        Truth.assertThat(list.update(0, "any")).isEqualTo("third");
+    }
+
+    @Test(timeout = 90000L, expected = Test.None.class)
+    public void testWithIntAndString() {
+        int head = 0;
+        String zero = "0";
+        int first = 2;
+        int second = 3;
+        int third = 4;
+        int end = 5;
+        String last = "5";
+
+
+        list.add(head);
+        Truth.assertThat(list.getHead()).isEqualTo(head);
+
+        list.appendToTheEnd(end);
+        Truth.assertThat(list.getEnd()).isEqualTo(end);
+
+        list.addAfter(second, 0);
+        Truth.assertThat(list.get(1)).isEqualTo(second);
+
+        list.addBefore(third, 2);
+        Truth.assertThat(list.get(2)).isEqualTo(third);
+
+        Truth.assertThat(list.delete(1)).isEqualTo(second);
+
+        Truth.assertThat(list.get(0)).isEqualTo(head);
+        Truth.assertThat(list.get(1)).isEqualTo(third);
+        Truth.assertThat(list.get(2)).isEqualTo(end);
+
+        Truth.assertThat(list.updateHead(zero)).isEqualTo(head);
+        Truth.assertThat(list.get(0)).isEqualTo(zero);
+        Truth.assertThat(list.updateEnd(last)).isEqualTo(end);
+        Truth.assertThat(list.get(2)).isEqualTo(last);
+
+        Truth.assertThat(list.deleteEnd()).isEqualTo(last);
+        Truth.assertThat(list.getEnd()).isEqualTo(third);
+
+        Truth.assertThat(list.deleteHead()).isEqualTo(zero);
+        Truth.assertThat(list.getEnd()).isEqualTo(third);
+        Truth.assertThat(list.getHead()).isEqualTo(third);
+
+        Truth.assertThat(list.update(0, "any")).isEqualTo(third);
+    }
+}
