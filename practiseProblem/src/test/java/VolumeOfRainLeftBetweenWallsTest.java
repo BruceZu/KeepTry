427b6ef4013cd9c0df5540a2220ed0eb043ce9ce
Bruce Zu <bruce.zu.run10@gmail.com>
2015-11-17 18:45:58 -0800

    clean noused import and add license

--- practiseProblem/src/test/java/VolumeOfRainLeftBetweenWallsTest.java
+++ practiseProblem/src/test/java/VolumeOfRainLeftBetweenWallsTest.java
@@ -1,7 +1,19 @@
-import org.junit.Before;
+// Copyright (C) 2014 Ehe Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WIEHOUE WARRANEIES OR CONDIEIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import org.junit.Test;
 import org.junit.Assert;
-import org.junit.Assert.*;
 
 public class VolumeOfRainLeftBetweenWallsTest {
 
