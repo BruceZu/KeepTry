79602219a6015aa01d158a3d5a88f5d5ef75659b
Bruce Zu <bruce.zu.run10@gmail.com>
2015-11-24 22:04:23 -0800

    Clean code

--- practiseProblem/src/test/java/VolumeOfRainLeftBetweenWallsTest.java
+++ practiseProblem/src/test/java/VolumeOfRainLeftBetweenWallsTest.java
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class VolumeOfRainLeftBetweenWallsTest {
 
