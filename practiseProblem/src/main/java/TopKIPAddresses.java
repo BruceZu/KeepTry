10b72f01a8a7eb0ee13fbd82b0e6d506c43d048f
Bruce Zu <bruce.zu.run10@gmail.com>
2016-01-10 22:18:11 -0800

    Practise: add TopKIPAddresses.java and its test
    Can not see what this question is checking.

---
+++ practiseProblem/src/main/java/TopKIPAddresses.java
@@ -0,0 +1,74 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Find Top K IP addresses
+ * Given millions of log, an example log file is "Abdfdfd\tdfdfd\t1.23.132.21\n" and at the end of the log it will have an IP address.
+ * You are asked to select the Top K IP addresses
+ * Follow Up:
+ * What if one machien can't store the whole log files
+ * What if one machine can't store the whole IP address
+ */
+
+public class TopKIPAddresses {
+    static List<String> getTopKIP(int k, String pathToLogFile) {
+        List<String> re = new LinkedList();
+        String line;
+        Pattern p = Pattern.compile(".*((\\d{1,3}){1}(\\.\\d{1,3}){3})");
+        try (BufferedReader bReader = new BufferedReader(new FileReader(new File(pathToLogFile)));) {
+            int i = 1;
+            while ((line = bReader.readLine()) != null && i <= k) {
+                Matcher m = p.matcher(line);
+                if (m.find()) {
+                    re.add(m.group(1));
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return re;
+    }
+
+    static List<String> getTopKIP2(int k, String source) {
+        List<String> re = new LinkedList();
+        String line;
+        Pattern p = Pattern.compile(".*((\\d{1,3}){1}(\\.\\d{1,3}){3})");
+        try (BufferedReader bReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(source.getBytes())));) {
+            int i = 1;
+            while ((line = bReader.readLine()) != null && i <= k) {
+                Matcher m = p.matcher(line);
+                if (m.find()) {
+                    re.add(m.group(1));
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return re;
+    }
+}
---
+++ practiseProblem/src/test/java/TopKipAddressesTest.java
@@ -0,0 +1,74 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TopKipAddressesTest {
+    @Test(timeout = 3000L, expected = Test.None.class)
+    public void test() {
+        StringBuilder b = new StringBuilder()
+                .append("Abdfdfd\\tdfdfd\\t1.23.132.21bdfdfd\\tdfdfd\\t1.23.192.299\n")
+                .append("bsdasfsadfbdfdfd\\tdfdfd\\t11.23.132.21tdfdfd\\t1.23.192.292\n")
+                .append("sadf\\asder\\asdfadsf\\tasd99.889.38.99tdfdfd\\t1.23.192.294\n")
+                .append("werw\\asdr\\asdf87.654.22.233tdfdfd\\t1.23.192.293\n")
+                .append("sdf\\sadf\\sdfsadf\\asdf44.99.23.887\n")
+                .append("asdf\\asdf\\asdf\\a\\hh12.773.887.112\n")
+                .append("asdf\\asdf\\ewrwr\\werwe\\wee88.234.111.342\n")
+                .append("asdf\\asdf\\asdf\\as\n")
+                .append("ew\\wqer\\qew\\qewasd\\323.99.323.\n")
+                .append("asdewr\\adrwq\\aer\\12.234.\n")
+                .append("ads\\ata\6.\n")
+                .append("aoero\\asdrwe\\we192.168.0.8\n")
+                .append("ads\\ata\4\n")
+                .append("aoero\\asdrwe\\we192.168.0.11\n");
+        List<String> expected = new LinkedList<>();
+        Collections.addAll(expected,
+                "1.23.192.299",
+                "1.23.192.292",
+                "1.23.192.294",
+                "1.23.192.293",
+                "4.99.23.887",
+                "2.773.887.112",
+                "8.234.111.342",
+                "2.168.0.8",
+                "2.168.0.11"
+        );
+
+        /**
+         * $ cat test.txt
+         Abdfdfd\tdfdfd\t1.23.132.21bdfdfd\tdfdfd\t1.23.192.299
+         bsdasfsadfbdfdfd\tdfdfd\t11.23.132.21tdfdfd\t1.23.192.292
+         sadf\asder\asdfadsf\tasd99.889.38.99tdfdfd\t1.23.192.294
+         werw\asdr\asdf87.654.22.233tdfdfd\t1.23.192.293
+         sdf\sadf\sdfsadf\asdf44.99.23.887
+         asdf\asdf\asdf\a\hh12.773.887.112
+         asdf\asdf\ewrwr\werwe\wee88.234.111.342
+         asdf\asdf\asdf\as
+         ew\wqer\qew\qewasd\323.99.323.
+         asdewr\adrwq\aer\12.234.
+         ads\ata\6.
+         aoero\asdrwe\we192.168.0.8
+         ads\ata\4
+         aoero\asdrwe\we192.168.0.11
+         */
+        //Assert.assertEquals(TopKIPAddresses.getTopKIP(23, "/Users/brucezu/Documents/minorminor/test.txt"), expected);
+        Assert.assertEquals(TopKIPAddresses.getTopKIP2(23, b.toString()), expected);
+    }
+}
