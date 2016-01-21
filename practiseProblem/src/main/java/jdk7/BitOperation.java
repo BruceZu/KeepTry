5118464521a8558abf0f8d051b18a3c88e402edb
Bruce Zu <bruce.zu.run10@gmail.com>
2016-01-20 18:55:44 -0800

    Add comments to BitOperation.binaryFormat()

--- practiseProblem/src/main/java/jdk7/BitOperation.java
+++ practiseProblem/src/main/java/jdk7/BitOperation.java
@@ -18,9 +18,18 @@ package jdk7;
 public class BitOperation {
     /**
      * @param input int number
-     * @return  binary string with 32 length
+     * @return  binary string in the format of two's complement with 32 length
      */
-    public static String bitFormat(int input) {
+    public static String binaryFormat(int input) {
+        // Java using two's complement to represent binary number. (sign-and-magnitude, ones' complement, two's complement, excess-N)
+        // the left first bit is sign bit.
+        // for positive integer, sign-and-magnitude= ones' complement= two's complement
+        // for negative integer, except the sign bit, two's complement = its Absolute value's ones' complement +1.
+
+        // for 8 bytes integer scope is [1111 1111 , 0111 1111], its [-127 , 127]
+        // 0 is[0000 0000] and [1000 0000]is -128,
+        // involve the sign bit in computing and let subtraction becomes addition.
+
         String in = Integer.toBinaryString(input);
         if (32 - in.length() > 0)
             return String.format("%0" + (32 - in.length()) + "d%" + in.length() + "s", 0, in);
