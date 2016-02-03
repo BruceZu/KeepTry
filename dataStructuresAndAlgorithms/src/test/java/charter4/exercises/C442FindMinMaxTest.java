fb57eced55b484e0039d60a4c2fc59e06e7200f4
Bruce Zu <bruce.zu.run10@gmail.com>
2016-02-02 18:58:14 -0800

    Update the test timeout value to 100L in C442FindMinMaxTest

--- dataStructuresAndAlgorithms/src/test/java/charter4/exercises/C442FindMinMaxTest.java
+++ dataStructuresAndAlgorithms/src/test/java/charter4/exercises/C442FindMinMaxTest.java
@@ -22,7 +22,7 @@ import java.util.Arrays;
 
 public class C442FindMinMaxTest {
 
-    @Test(timeout = 10000L, expected = Test.None.class)
+    @Test(timeout = 100L, expected = Test.None.class)
     public void test() {
         Assert.assertEquals(Arrays.toString(C442FindMinMax.findMinimumAndMaximumOf(new int[]{9, 3, 1, 6, 7, 4, 10})), "[10, 1]");
         Assert.assertEquals(Arrays.toString(C442FindMinMax.findMinimumAndMaximumOf(new int[]{9, 3, 2, 6, 1, 4})), "[9, 1]");
