a5f3d65240a610dae56381332551981a488d6eba
Bruce Zu <bruce.zu.run10@gmail.com>
2016-02-02 19:00:18 -0800

    Add C520OddAheadEvenArray and its Junit test

---
+++ dataStructuresAndAlgorithms/src/main/java/charter5/exercises/C520OddAheadEvenArray.java
@@ -0,0 +1,72 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package charter5.exercises;
+
+// rearranges an array of integer values so that all the even values appear before all the odd values.
+public class C520OddAheadEvenArray {
+    // recursive method, running time O(N)
+    private static void rearranges(int[] array, int indexEven, int currentIndex) {
+        if (currentIndex == array.length) {
+            return;
+        }
+
+        int who = array[currentIndex] & 1;
+        if (who == 0 && indexEven == -1) {
+            indexEven = currentIndex;
+        }
+        if (who == 1 && indexEven != -1) {
+            // swap
+            array[indexEven] ^= array[currentIndex];
+            array[currentIndex] ^= array[indexEven];
+            array[indexEven] ^= array[currentIndex];
+            indexEven++; //
+        }
+
+        rearranges(array, indexEven, currentIndex + 1);
+    }
+
+
+
+    public static void rearranges(int[] array) {
+        rearranges(array, -1, 0);
+    }
+
+    // no recursion implementation, running time O(N)
+    public static void rearranges2(int[] array) {
+        int indexEven = -1;
+        for (int i = 0; i < array.length; i++) {
+            int check = (array[i] & 1);
+            //    even
+            //odd even
+            if (check == 0 && indexEven == -1) {
+                indexEven = i;
+                continue;
+            }
+            // even odd
+            if (check == 1 && indexEven != -1) {
+                // swap
+                array[indexEven] ^= array[i];
+                array[i] ^= array[indexEven];
+                array[indexEven] ^= array[i];
+
+                indexEven++; //
+            }
+            // even even
+            // odd  odd
+            //      odd
+        }
+    }
+}
---
+++ dataStructuresAndAlgorithms/src/test/java/charter5/exercises/C520OddAheadEvenArrayTest.java
@@ -0,0 +1,35 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package charter5.exercises;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class C520OddAheadEvenArrayTest {
+    @Test(timeout = 10L, expected = Test.None.class)
+    public void test() {
+        int[] test = new int[]{2, 6, 8, 1, 3, 4, 5, 6, 7, 8, 12, 1, 22, 9, 9, 9};
+        int[] expected = new int[]{1, 3, 5, 7, 1, 9, 9, 9, 2, 8, 12, 6, 22, 4, 8, 6};
+        C520OddAheadEvenArray.rearranges(test);
+        Assert.assertEquals(Arrays.equals(test, expected), true);
+
+        test = new int[]{2, 6, 8, 1, 3, 4, 5, 6, 7, 8, 12, 1, 22, 9, 9, 9};
+        C520OddAheadEvenArray.rearranges2(test);
+        Assert.assertEquals(Arrays.equals(test, expected), true);
+    }
+}
