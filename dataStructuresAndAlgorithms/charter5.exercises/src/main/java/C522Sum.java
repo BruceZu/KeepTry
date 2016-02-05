0d91162286e53b3b0daa6797d3dfa7a4bacea541
Bruce Zu <bruce.zu.run10@gmail.com>
2016-02-04 21:54:33 -0800

    Add C522Sum and its Junit test

---
+++ dataStructuresAndAlgorithms/charter5.exercises/src/main/java/C522Sum.java
@@ -0,0 +1,56 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/*
+C-5.22 Suppose you are given an array, A, containing n distinct integers that are listed
+in increasing order. Given a number k, describe a recursive algorithm to find two
+integers in A that sum to k, if such a pair exists. What is the running time of your
+algorithm?
+ */
+
+
+public class C522Sum {
+    private static void find(int[] indexes, int[] nums, long k) {
+        // walk from both sides towards center.
+        // index[0] keep left side index, index[1] keep right side index,
+        // runtime O(N)
+        int l = indexes[0];
+        int r = indexes[1];
+        if (l == r) {
+            indexes[0] = -1;
+            indexes[1] = -1;
+            return;
+        }
+        if (nums[l] + nums[r] == k) {
+            return;
+
+        }
+        if (nums[l] + nums[r] < k) {
+            indexes[0]++;
+            find(indexes, nums, k);
+            return;
+        }
+        indexes[1]--;
+        find(indexes, nums, k);
+    }
+
+    public static int[] find(int[] nums, int k) {
+        int[] r = new int[2];
+        r[0] = 0;
+        r[1] = nums.length - 1;
+        find(r, nums, k);
+        return r;
+    }
+}
---
+++ dataStructuresAndAlgorithms/charter5.exercises/src/test/java/C522SumTest.java
@@ -0,0 +1,29 @@
+//  Copyright 2016 The Minorminor Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class C522SumTest {
+    @Test(timeout = 10L, expected = Test.None.class)
+    public void test() {
+        int[] r = C522Sum.find(new int[]{1, 9, 11, 21, 23}, 20);
+        Assert.assertEquals(r[0], 1);
+        Assert.assertEquals(r[1], 2);
+        r = C522Sum.find(new int[]{1, 9, 11, 21, 23}, 19);
+        Assert.assertEquals(r[0], -1);
+        Assert.assertEquals(r[1], -1);
+    }
+}
