fa10f4a2405f09488e1215cdff0928f15e4c4db1
Bruce Zu <bruce.zu.run10@gmail.com>
2016-02-17 20:56:50 -0800

    Add comments to Leetcode86PartitionList

--- practiseProblem/leetcode/src/main/java/Leetcode86PartitionList.java
+++ practiseProblem/leetcode/src/main/java/Leetcode86PartitionList.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+
 /*
 Given a linked list and a value x, partition it such that all nodes less than x
 come before nodes greater than or equal to x.
@@ -20,44 +22,47 @@ For example,
 Given 1->4->3->2->5->2 and x = 3,
 return 1->2->2->4->3->5.
  */
-
+// https://leetcode.com/problems/partition-list/
 public class Leetcode86PartitionList {
     public class Solution {
         // beats 5.06% of java submissions on Feb 10, 2016
-        private void process(ListNode node, int x, ListNode[] ts) {
+        private void process(ListNode node, int x, ListNode[] tNodes) {
             if (node != null) {
                 if (node.val < x) {
-                    ts[0].next = node;
-                    ts[0] =ts[0].next;
+                    tNodes[0].next = node;
+                    tNodes[0] = tNodes[0].next;
                 } else {
-                    ts[1].next = node;
-                    ts[1] = ts[1].next;
+                    tNodes[1].next = node;
+                    tNodes[1] = tNodes[1].next;
                 }
-               process(node.next, x, ts);
+                process(node.next, x, tNodes);
             }
         }
 
+        // recursion
         public ListNode partition(ListNode head, int x) {
             ListNode smalsDumyHead = new ListNode(0);
             ListNode smalsTail = smalsDumyHead;
             ListNode bigsDumyHead = new ListNode(0);
             ListNode bigsTail = bigsDumyHead;
 
-            ListNode [] ts = new ListNode []{smalsTail,bigsTail};
-            process( head, x, ts);
+            ListNode[] tailNodes = new ListNode[]{smalsTail, bigsTail};
+            process(head, x, tailNodes);
 
-            ts[0].next = bigsDumyHead.next;
-            ts[1].next = null; // note
+            tailNodes[0].next = bigsDumyHead.next;
+            tailNodes[1].next = null; // note
             return smalsDumyHead.next;
         }
 
         // beats 5.06% of java submissions on Feb 10, 2016
+        // loop
         public ListNode partition2(ListNode head, int x) {
             ListNode smalsDumyHead = new ListNode(0);
             ListNode smalsTail = smalsDumyHead;
             ListNode bigsDumyHead = new ListNode(0);
             ListNode bigsTail = bigsDumyHead;
 
+            // divide it into 2 list
             ListNode cur = head;
             while (cur != null) {
                 if (cur.val < x) {
@@ -70,6 +75,7 @@ public class Leetcode86PartitionList {
                 cur = cur.next;
             }
 
+            // append the bigger list to smaller list
             smalsTail.next = bigsDumyHead.next;
             bigsTail.next = null; // note
             return smalsDumyHead.next;
