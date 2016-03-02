d5eba6e1fdd76f6e23923ab9cbf1a648e1928986
Bruce Zu <bruce.zu.run10@gmail.com>
2016-03-01 17:35:56 -0800

    Comments on C621PermutationWithStack
    Change-Id: I05e1e7ef593361c1ca09f4179694b6f29de109e3

--- dataStructuresAndAlgorithms/charter6.exercises/src/main/java/permutation/C621PermutationWithStack.java
+++ dataStructuresAndAlgorithms/charter6.exercises/src/main/java/permutation/C621PermutationWithStack.java
@@ -16,9 +16,29 @@
 package permutation;
 
 /**
- * C-6.21 Describe a nonrecursive algorithm for enumerating all permutations of the numbers
+ * C-6.21
+ * Describe a nonrecursive algorithm for enumerating all permutations of the numbers
  * {1,2, . . . ,n} using an explicit stack.
+ * <p/>
+ * Catalan numbers.
+ * the number of permutation of n numbers is n!, (pronounced "N factorial").
+ * refer to
+ * http://www.labri.fr/perso/bousquet/Exposes/versailles.pdf
+ * http://ioctl.org/jan/friday/stacks
+ * http://www.cplusplus.com/reference/algorithm/next_permutation/
+ * http://lh3.ggpht.com/_bLHHR6rd5Ug/Sxn2isijPNI/AAAAAAAAAK8/1oSWhhjB7AI/s1600-h/AlgorithmL20.gif
+ * http://stackoverflow.com/questions/6479779/generate-permutation-using-a-single-stack/6481240#6481240?newreg=ffe465a5e49445bcae27966b05877d09
+ * https://math.dartmouth.edu/~doyle/docs/knuth/knuth.pdf
+ * http://www-circa.mcs.st-and.ac.uk/~sal/DequesAndStacks.pdf
+ * http://math.stackexchange.com/questions/532520/prove-why-this-algorithm-to-compute-all-list-permutations-works
+ * http://math.stackexchange.com/questions/17769/using-one-stack-to-find-number-of-permutations
+ * http://math.stackexchange.com/questions/1405316/manual-generation-of-all-permutations-of-n-non-repeating-elements
+ * In general, any recursive algorithm can always be reduced to
+ * an iterative one through the use of stack or queue data structures.
  */
 public class C621PermutationWithStack {
     // To-do
+    // I think this question is wrong it should be
+    // Describe a nonrecursive algorithm for enumerating all Catalan numbers permutations of the numbers
+    // {1,2, . . . ,n} using an explicit stack.
 }
