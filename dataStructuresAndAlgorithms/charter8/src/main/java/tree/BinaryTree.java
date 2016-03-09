793ade34cf4f1ba69573a40eb53e0c045819f143
Bruce Zu <bruce.zu.run10@gmail.com>
2016-03-09 14:39:40 -0800

    Add BinaryTree
    Change-Id: Iafcb7d734dcb22a190688e412904f97d55b41311

---
+++ dataStructuresAndAlgorithms/charter8/src/main/java/tree/BinaryTree.java
@@ -0,0 +1,56 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package tree;
+
+// A binary tree is an ordered tree
+// Let T be a nonempty binary tree, and
+//
+// n: the number of nodes,
+// nE: number of external nodes,
+// nI: number of internal nodes,
+// h:  height of T, respectively.
+//
+// depth                 level
+//
+// 0         o           2^0              o               o
+//          / \                          / \             / \
+// 1       o   o         2^1            o   o           o   o
+//        / \ / \                      / \ /           / \
+// 2     o  oo   o       2^2          o  oo           o  o
+//
+// The set of all nodes of a tree T at the same depth d: 'level' d of T
+//
+//        1. h+1 ≤ n ≤ 2^(h+1)−1
+//        2. 1 ≤ nE ≤ 2^h
+//        3. h ≤ nI ≤ 2^h−1
+//        4. log(n+1)−1 ≤ h ≤ n−1
+//
+// if T is proper:
+//        1. 2h+1 ≤ n ≤ 2^(h+1)−1
+//        2. h+1 ≤ nE ≤ 2^h
+//        3. h ≤ nI ≤ 2^h−1
+//        4. log(n+1)−1 ≤ h ≤ (n−1)/2
+//
+// a nonempty proper binary tree T:
+//           nE = nI +1.
+//
+public interface BinaryTree<T> extends Tree<T> {
+    TreeNode<T> left(TreeNode<T> n);
+
+    TreeNode<T> right(TreeNode<T> n);
+
+    TreeNode<T> sibling(TreeNode<T> n);
+}
