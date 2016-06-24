d3b2281ad4630afba1f409e1af5af29ac5a0fd7c
Bruce Zu <bruce.zu.run10@gmail.com>
2016-06-23 22:27:14 -0700

    Leetcode 26 Remove Duplicates from Sorted Array
    Change-Id: I1ee26ca18b33da0dc556ba95897916cc7bbb60e3

---
+++ dataStructuresAndAlgorithms/src/main/java/array/doublepoint/Leetcode26RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,44 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package array.doublepoint;
+
+/**
+ * Difficulty: Easy <pre>
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * For example,
+ * Given input array nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length.
+ * Subscribe to see which companies asked this question
+ * Hide Tags Array Two Pointers
+ * Hide Similar Problems (E) Remove Element
+ * @see <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/">leetcode</a>
+ */
+public class Leetcode26RemoveDuplicatesfromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int nextDistinguishIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[nextDistinguishIndex - 1]) {
+                nums[nextDistinguishIndex++] = nums[i];
+            }
+        }
+        return nextDistinguishIndex;
+    }
+}
