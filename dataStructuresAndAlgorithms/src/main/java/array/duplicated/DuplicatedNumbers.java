c1e13607b6b87e0eccf1f04b2221b7d8d9c4b18d
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-01 12:05:39 -0700

    More than 2 duplicated numbers
    Change-Id: Id41be027353691e56c2c32640872a532447d04e0

---
+++ dataStructuresAndAlgorithms/src/main/java/array/duplicated/DuplicatedNumbers.java
@@ -0,0 +1,37 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package array.duplicated;
+
+public class DuplicatedNumbers {
+    /**
+     * <pre>
+     * Given an array of n elements which contains elements from 0 to n-1,
+     * with any of these numbers appearing any number of times.
+     * Find these repeating numbers in O(n) and using only constant memory space.
+     *
+     * For example:
+     *      let n be 7 and array be {1, 2, 3, 1, 3, 6, 6},
+     *      the answer should be 1, 3 and 6.
+     *
+     *
+     * Idea:
+     *   All elements > 0; So => value as index, check times.
+     *   n elements from 0 to n-1. So => value as index, check sign.
+     */
+    public static void duplicatedNums(int[] nums) {
+
+    }
+}
