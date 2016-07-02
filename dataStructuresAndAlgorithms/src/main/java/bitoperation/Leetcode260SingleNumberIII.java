7e548d761a2dcb3ff95bd7f36941ceb1fd92b990
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-01 23:15:13 -0700

    Leetcode 260 Single Number III
    Change-Id: I96252c43b57f4f2f11c466dad0533cd1d105b644

--- dataStructuresAndAlgorithms/src/main/java/bitoperation/Leetcode260SingleNumberIII.java
+++ dataStructuresAndAlgorithms/src/main/java/bitoperation/Leetcode260SingleNumberIII.java
@@ -15,7 +15,45 @@
 
 package bitoperation;
 
-public class NoRepeated2Num {
+/**
+ * <pre>
+ * Difficulty: Medium
+ * Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+ *
+ * For example:
+ *
+ * Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+ *
+ * Note:
+ *      The order of the result is not important. So in the above example, [5, 3] is also correct.
+ *      Your algorithm should run in linear runtime complexity.
+ *      Could you implement it using only constant space complexity?
+ *
+ * Tags: Bit Manipulation
+ * Similar Problems
+ * (M) Single Number
+ * (M) Single Number II
+ */
+public class Leetcode260SingleNumberIII {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+        int divideBy = xor & (-xor);
+        int[] r = new int[2];
+        r[0] = xor;
+        r[1] = xor;
+        for (int i = 0; i < nums.length; i++) {
+            int v = nums[i];
+            if ((divideBy & v) == divideBy) {
+                r[0] ^= v;
+            } else {
+                r[1] ^= v;
+            }
+        }
+        return r;
+    }
 
     /**
      * <pre>
