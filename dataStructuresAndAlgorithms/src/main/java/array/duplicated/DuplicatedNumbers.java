2e3cc699674c0e545fbc3b2686368b6c233f141a
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-07 15:08:32 -0700

    Check duplicated numbers by sign mark or swap
    Change-Id: Ie408532f97bcd94b9d265425aaf6946005e9b1fd

--- dataStructuresAndAlgorithms/src/main/java/array/duplicated/DuplicatedNumbers.java
+++ dataStructuresAndAlgorithms/src/main/java/array/duplicated/DuplicatedNumbers.java
@@ -15,7 +15,18 @@
 
 package array.duplicated;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DuplicatedNumbers {
+    private static int[] toArray(List<Integer> l) {
+        int[] r = new int[l.size()];
+        for (int i = 0; i < l.size(); i++) {
+            r[i] = l.get(i);
+        }
+        return r;
+    }
+
     /**
      * <pre>
      * Given an array of n elements which contains elements from 0 to n-1,
@@ -28,10 +39,87 @@ public class DuplicatedNumbers {
      *
      *
      * Idea:
-     *   All elements > 0; So => value as index, check times.
-     *   n elements from 0 to n-1. So => value as index, check sign.
+     *  1  All elements > 0; So => value as index, check times.
+     *  2  n elements from 0 to n-1.
+     *     So => if we can update the array:
+     *        21 value as index, check sign.
+     *        22 swap;
+     *     Runtime O(N)
+     *
+     *   index   0 1  2 3  4 5 6
+     *                             current index
+     *   nums:   2 4  6 4  1 0 5      0
+     *           6 4  2 4  1 0 5      0
+     *           5 4  2 4  1 0 6      0
+     *           0 4  2 4  1 5 6      -> 1
+     *           0 1  2 4  4 5 6      -> 3,4,5,6
+     *                  |
+     *                 4 is duplicated and  3 is missing.
+     *
+     *                                 current index
+     *
+     *   index   0  1  2  3  4  5  6
+     *
+     *    nums   1, 2, 3, 1, 3, 6, 6     0
+     *           2, 1, 3, 1, 3, 6, 6     0
+     *           3, 1, 2, 1, 3, 6, 6     0
+     *           1, 1, 2, 3, 3, 6, 6     0
+     *           |           |  |         0,1,2,3,4,5,6
      */
-    public static void duplicatedNums(int[] nums) {
+    public static int[] duplicatedNums(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return null;
+        }
+        List<Integer> dups = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (true) {
+                if (i == nums[i]) {
+                    break;
+                }
+                int vIndex = nums[i];
+                if (vIndex == nums[vIndex]) {
+                    dups.add(vIndex);
+                    break;
+                }
+                nums[i] ^= nums[vIndex];
+                nums[vIndex] ^= nums[i];
+                nums[i] ^= nums[vIndex];
+            }
+        }
+        return toArray(dups);
+    }
 
+    /**
+     * <pre>
+     * Note:  Maybe the nums[i] is already marked as negative number.
+     *
+     *   index   0 1 2 3 4 5 6
+     *   nums:   2 4 6 4 1 0 5
+     *   mark:  -2-4-6 4-1-0-5
+     *                 |
+     *                 4 is duplicated and 3 is missing.
+     *
+     *
+     *   index   0  1  2  3  4  5  6
+     *    nums   1, 2, 3, 1, 3, 6, 6     0
+     *    mark   1,-2,-3,-1, 3, 6,-6
+     *                    |  |     |
+     */
+    public static int[] duplicatedNumsByMarkSign(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return null;
+        }
+        List<Integer> dups = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int vIndex = nums[i];
+            vIndex = Math.abs(vIndex); // Note
+            if (nums[vIndex] < 0) {
+                dups.add(vIndex);
+                continue;
+            }
+            nums[vIndex] *= -1;
+        }
+
+        return toArray(dups);
     }
 }
