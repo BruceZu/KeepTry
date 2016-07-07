755821112bb7419ae7687bfd9d2720181684bf18
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-06 21:44:45 -0700

    Random by Random
    Change-Id: I3b293a2a4476440f741b3ae7b7da983d93d013e2

---
+++ dataStructuresAndAlgorithms/src/main/java/random/RandomByRandom.java
@@ -0,0 +1,58 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package random;
+
+import java.util.Random;
+
+/**
+ * using a rand5 function design a rand7 function
+ * using a rand7 function design a rand10 function
+ */
+public class RandomByRandom {
+    private static Random r = new Random();
+
+    public static int random5() {
+        return r.nextInt(5);
+    }
+
+    /**
+     * <pre>
+     *    random5 : 0, 1, 2, 3, 4
+     * A  So using 5 * random5() to making sure r is uniformly distributed.
+     *      < 5 : over lap.
+     *      > 5 : there is gap.
+     *
+     *         step: 5
+     *      So the r is [ 0, 1, 2, 3, 4;      0
+     *                    5, 6, 7, 8, 9;      1
+     *                   10,11,12,13,14;      2
+     *                   15,16,17,18,19;      3
+     *                   20,21,22,23,24 ]     4
+     *      So at most it can construct random25(). or add more like:
+     *         25 * random5() + 5 * random5() + random5()
+     *
+     *      For those < 25, to make sure the result is uniformly distributed:
+     * B    need cut some element
+     * C    need %
+     */
+    public static int random7() {
+        int r = 5 * random5() + random5();
+        while (r >= 21) {
+            r = 5 * random5() + random5();
+        }
+        return r % 7;
+    }
+}
