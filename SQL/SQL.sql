d37917cb700391f2a27cbb046d70447ef83ee65d
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-14 17:55:00 -0700

    A eBay interview sql question
    Change-Id: If6ffdaca82018aa5f89b361636783e8a99667daa

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -4,8 +4,24 @@
     * (rownum is Oracle SQL)
     */
 
-select orderNumber, orderDate, requiredDate, shippedDate, status,  comments, customerNumber
+ select orderNumber, orderDate, requiredDate, shippedDate, status,  comments, customerNumber
  from orders
  where  status <> 'Shipped'  and rownum <= 5
  order by orderDate asc;
+
+
+
+ /**
+  * table A和B是一对多的关系，求A对应在B的某column的avg。
+  * http://www.w3schools.com/sql/trysql.asp?filename=trysql_select_join
+  * =: have no space on both side
+  * join need on
+  * group by is at last
+  * CustomerID need specify its table name
+  */
+ select Customers.CustomerID, OrderID, avg(EmployeeID)
+ from  Customers left join orders
+ on Customers.customerid=orders.customerid
+ where Customers.customerid in ('5', '4')
+ group by Customers.CustomerID
 
