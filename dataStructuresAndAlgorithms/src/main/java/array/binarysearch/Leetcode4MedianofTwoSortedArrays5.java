cda45a71a73442005408239c82517d82a5a77c2d
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-25 17:41:31 -0700

    todo other ideas on Leetcode 4 median of two sorted arrays
    Change-Id: Iae0715afbe3b41a79ca173a5412c84fcfff48415

---
+++ dataStructuresAndAlgorithms/src/main/java/array/binarysearch/Leetcode4MedianofTwoSortedArrays5.java
@@ -0,0 +1,27 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package array.binarysearch;
+
+public class Leetcode4MedianofTwoSortedArrays5 {
+    /**
+     * @see <a href ="https://leetcode.com/discuss/41621/very-concise-iterative-solution-with-detailed-explanation"> Idea 1 </a>
+     *       <a href="https://leetcode.com/discuss/15790/share-my-o-log-min-m-n-solution-with-explanation"> Idea 2 </a>
+     */
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        //todo
+        return 0;
+    }
+}
