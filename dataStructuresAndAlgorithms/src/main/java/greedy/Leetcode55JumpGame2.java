d926758a01cf27f5df94e8f70afaf6168b23fe22
Bruce Zu <bruce.zu.run10@gmail.com>
2016-07-30 15:31:51 -0700

    Polish
    Change-Id: I3161c21b618bf9df6b9f7832e98d5733ec5b5014

--- dataStructuresAndAlgorithms/src/main/java/greedy/Leetcode55JumpGame2.java
+++ dataStructuresAndAlgorithms/src/main/java/greedy/Leetcode55JumpGame2.java
@@ -15,32 +15,7 @@
 
 package greedy;
 
-/**
- * <pre>
- * 55. Jump Game
- * Difficulty: Medium
- * Given an array of non-negative integers, you are initially positioned at the first index of the array.
- *
- * Each element in the array represents your maximum jump length at that position.
- *
- * Determine if you are able to reach the last index.
- *
- * For example:
- * A = [2,3,1,1,4], return true.
- *
- * A = [3,2,1,0,4], return false.
- *
- * Company Tags Microsoft
- * Tags Array Greedy
- * ----------------------------------------------------------------------------
- *
- *  the cost of checking does not deserve the benefit:
- *      if (greedyTo >= nums.length - 1) {
- *          return true;
- *      }
- */
-public class Leetcode55JumpGame {
-
+public class Leetcode55JumpGame2 {
     public boolean canJump(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return true;
@@ -48,18 +23,11 @@ public class Leetcode55JumpGame {
         int atlest = 1;
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] >= atlest) {
+                atlest = 1;
                 continue;
             }
-            while (i >= 0 && nums[i] < atlest) {
-                atlest++;
-                i--;
-            }
-            if (i == -1) {
-                return false;
-            }
-            atlest = 1;
+            atlest++;
         }
         return atlest == 1;
     }
 }
-
