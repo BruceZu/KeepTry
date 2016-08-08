418217c17030699a26a42bc367effb2a13bc7126
Bruce Zu <bruce.zu.run10@gmail.com>
2016-08-07 19:06:25 -0700

    Leetcode 253 Meeting Rooms
    Change-Id: I1f0a64f86691b9908760afd45207bdaa8608dce9

--- dataStructuresAndAlgorithms/src/main/java/sort/Leetcode252MeetingRooms.java
+++ dataStructuresAndAlgorithms/src/main/java/sort/Leetcode252MeetingRooms.java
@@ -13,68 +13,53 @@
 // limitations under the License.
 //
 
-package nosubmmitted;
+package sort;
 
 import java.util.Arrays;
 
 /**
+ * <pre>
  * 252. Meeting Rooms
  * https://leetcode.com/problems/meeting-rooms/
  * Difficulty: Easy
- * Given an array of meeting time intervals consisting of start and
- * end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
- * <p/>
- * For example,
- * Given [[0, 30],[5, 10],[15, 20]],
+ * Given an array of meeting time intervals consisting of start and end times
+ *      [[s1,e1],[s2,e2],...] (si < ei)
+ *
+ *  determine if a person could attend all meetings.
+ *
+ * For example, Given
+ *      [[0, 30],[5, 10],[15, 20]]
+ *
  * return false.
- * <p/>
- * Hide Company Tags: Facebook
- * Hide Tags: Sort
- * Hide Similar Problems (H) Merge Intervals (M) Meeting Rooms II
+ *
+ *   Company Tags: Facebook
+ *   Tags: Sort
+ *   Similar Problems
+ *      (H) Merge Intervals (M)
+ *      Meeting Rooms II
  */
 
-public class LC252MeetingRooms {
-    /**
-     * Definition for an interval.
-     */
+public class Leetcode252MeetingRooms {
     class Interval {
         int start;
         int end;
-
-        Interval() {
-            start = 0;
-            end = 0;
-        }
-
-        Interval(int s, int e) {
-            start = s;
-            end = e;
-        }
     }
 
-    /**
-     * the fast one should beat 99%  not shared code
-     */
-
-    /**
-     * beat 87.96%
-     */
     public boolean canAttendMeetings(Interval[] intervals) {
         int len = intervals.length;
         if (len == 0) {
             return true;
         }
-        int[] begin = new int[len];
-        int[] stop = new int[len];
+        int[] starts = new int[len];
+        int[] ends = new int[len];
         for (int i = 0; i < len; i++) {
-            begin[i] = intervals[i].start;
-            stop[i] = intervals[i].end;
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
         }
-        Arrays.sort(begin);
-        Arrays.sort(stop);
-        int endT = 0;
+        Arrays.sort(starts);
+        Arrays.sort(ends);
         for (int i = 1; i < len; i++) {
-            if (begin[i] < stop[i - 1]) {
+            if (starts[i] < ends[i - 1]) {
                 return false;
             }
         }
