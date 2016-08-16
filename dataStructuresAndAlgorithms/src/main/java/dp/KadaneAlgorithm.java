688a6b0c983aa2f69b044a25bc64d1f563e5a4c0
Bruce Zu <bruce.zu.run10@gmail.com>
2016-08-16 13:47:37 -0700

    Polish Kadane Algorithm
    Change-Id: I2c3bc7fca09cf3d7cf4534012887553041f4064e

--- dataStructuresAndAlgorithms/src/main/java/dp/KadaneAlgorithm.java
+++ dataStructuresAndAlgorithms/src/main/java/dp/KadaneAlgorithm.java
@@ -19,64 +19,130 @@ package dp;
  * <pre>
  *  Kadane Algorithm -> Maximum subarray problem
  *  Jay Kadane of Carnegie-Mellon University (Bentley 1984).
- * O(n) in time complexity and memory usage
+ *
+ *  O(n) in time complexity and memory usage
  *
  * when all the elements are negative values?
  * 1    use a variable (max_singleelement).
- *      At the end,
- *      if this value is lower than 0 then return max_single_element
+ *      At the end, if this value is lower than 0 then return max_single_element
  *      otherwise return maxSoFar;
  *
- * 2    improved version // Statement A ->
+ * 2    improved version:
+ *      maxCur = Math.max(0     , maxCur += arr[i]);
+ *      ->
  *      maxCur = Math.max(arr[i], maxCur += arr[i]);
  *                           |
+ *  why does Kadane Algorithm work?
+ *     why the the peak is the position where the Maximum subarray should end.
+ *     why the position is the right start position, from where start a serial positive sum value, just after
+ *     the pre position where sum value is <=0
+ *     case of improved version:
+ *     [-2, -3, 4, -1, -2, 1, 5, -3, 4 ]
+ *     [-2]*
+ *         [-3]
+ *            [ 4]*
+ *            [    3 ]
+ *            [        1]
+ *            [            2]
+ *            [              7]*
+ *            [                 4]
+ *            [                     8]* peak
  *
- * Test case: [-2, -3, 4, -1, -2, 1, 5, -3, 4 ] -> 8
- *              0  4   3   1  2  7   4  8
- *              -3  4   3   1  2  7   4  8 //improved version
+ *    case of improved version:
+ *     [0, -1, -2, -1, 0, 1, 2, 1, 0, -1, -2, -1, 0 ]
+ *     [0]*
+ *     [  -1]
+ *          [ -2 ]
+ *               [-1]
+ *                   [0]
+ *                   [   1]*
+ *                   [     3]*
+ *                   [        4]* peak end
+ *                   [            4]
+ *                   [               3]
+ *                   [                   1]
+ *                   [                       0]
+ *                   [                          0]
+ *     case of improved version:
+ *        [-1, -2, -2, -1, -3]
+ *        [-1]* peak
+ *            [-2]
+ *               [-2]
+ *                   [-1]
+ *                       [-3]
  *
- *            [- 1,  - 2,  - 2,  - 1,  - 3]
- *              0     0      0     0   -> max_single_element = -1
- *              -2    -2    -1     -3  -> maxSoFar = -1; //improved version
+ *
+ *  Test case:
+ *    maximum:
+ *     -2, 1, -3, 4, -1, 2, 1, -5, 4                -> 6
+ *     -2, -3, 4, -1, -2, 1, 5, -3, 4               -> 8
+ *     -1, -2, -2, -1, -3                           -> -1
+ *     1, 2, 2, 1, 3                                -> 9
+ *     0, -1, -2, -1, 0, 1, 2, 1, 0, -1, -2, -1, 0  -> 4
+ *
+ *   mininum:
+ *     -2, 1, -3, 4, -1, 2, 1, -5, 4                -> -5
+ *     -2, -3, 4, -1, -2, 1, 5, -3, 4               -> -5
+ *     -1, -2, -2, -1, -3                           -> -9
+ *     1, 2, 2, 1, 3                                -> 1
+ *     0, -1, -2, -1, 0, 1, 2, 1, 0, -1, -2, -1, 0  -> -4
+ *
+ *   more test case @see <a href="https://leetcode.com/problems/maximum-subarray/">leetcode</a>
  *
  *  @see <a href="https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane.27s_algorithm">Kadane's algorithm</a>
  */
 public class KadaneAlgorithm {
-    public static int go(int[] arr) {
-        int maxCur = arr[0], maxSoFar = arr[0];
+    public static int maximum(int[] arr) {
+        int maxEndsHere = arr[0], peak = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
-            maxCur = Math.max(arr[i], maxCur += arr[i]); // Statement A, improved version
-            maxSoFar = Math.max(maxCur, maxSoFar);
+            maxEndsHere = Math.max(arr[i], maxEndsHere += arr[i]);
+            peak = Math.max(maxEndsHere, peak);
         }
-        return maxSoFar;
+        return peak;
     }
 
-    public static int go2(int[] arr) {
-        int min = 0;
-        int max = arr[0], sum = arr[0];
+    // Alternative
+    public static int maximum2(int[] arr) {
+        int bottom = 0;
+        int rightDelta = arr[0], sum = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
             sum += arr[i];
-            if (sum - min > max) {
-                max = sum - min;
+            if (sum - bottom > rightDelta) {
+                rightDelta = sum - bottom;
             }
-            if (sum < min) {
-                min = sum;
+            if (sum < bottom) {
+                bottom = sum;
             }
         }
-        return max;
+        return rightDelta;
     }
 
-    public static void main(String[] args) {
-        System.out.println(go(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
-        System.out.println(go(new int[]{-2, -3, 4, -1, -2, 1, 5, -3, 4}));
-        System.out.println(go(new int[]{-1, -2, -2, -1, -3}));
-        System.out.println(go(new int[]{1, 2, 2, 1, 3}));
-        System.out.println();
-        System.out.println(go2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
-        System.out.println(go2(new int[]{-2, -3, 4, -1, -2, 1, 5, -3, 4}));
-        System.out.println(go2(new int[]{-1, -2, -2, -1, -3}));
-        System.out.println(go2(new int[]{1, 2, 2, 1, 3}));
+    public static int minimum(int[] arr) {
+        int maxEndsHere = arr[0], peak = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndsHere = Math.min(arr[i], maxEndsHere += arr[i]);
+            peak = Math.min(maxEndsHere, peak);
+        }
+        return peak;
+    }
+
+    // Alternative
+    public static int minimum2(int[] arr) {
+        int top = 0;
+        int rightDelta = arr[0], sum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum - top < rightDelta) {
+                rightDelta = sum - top;
+            }
+            if (sum > top) {
+                top = sum;
+            }
+        }
+        return rightDelta;
     }
 }
