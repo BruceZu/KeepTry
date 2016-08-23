5fa846801872594ac253573f72b96fcc5c8f658b
Bruce Zu <bruce.zu.run10@gmail.com>
2016-08-22 22:25:11 -0700

    Leetcode 45 Jump Game II
    Change-Id: I87ad4cfed80d8c4f09afcdea29fdf13c8e65c249

---
+++ dataStructuresAndAlgorithms/src/main/java/greedy/Leetcode45JumpGameII.java
@@ -0,0 +1,41 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package greedy;
+
+/**
+ * <pre>  45. Jump Game II
+ * Difficulty: Hard
+ *
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Your goal is to reach the last index in the minimum number of jumps.
+ *
+ * For example:
+ * Given array A = [2,3,1,1,4]
+ *
+ * The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+ *
+ * Note:
+ * You can assume that you can always reach the last index.
+ *
+ *
+ *  Tags Array Greedy
+ */
+public class Leetcode45JumpGameII {
+
+}
