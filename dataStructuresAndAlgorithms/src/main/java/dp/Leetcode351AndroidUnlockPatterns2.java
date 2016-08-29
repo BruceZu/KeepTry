e6e96e8ee699a316d433080b4665a2fac8462fa9
Bruce Zu <bruce.zu.run10@gmail.com>
2016-08-28 18:47:36 -0700

    other of deciding nexts of DFS for Leetcode351AndroidUnlockPatterns
    Change-Id: Ib6fefe794415588bdbcfb9bcd9318050c4dccfc2

--- dataStructuresAndAlgorithms/src/main/java/dp/Leetcode351AndroidUnlockPatterns2.java
+++ dataStructuresAndAlgorithms/src/main/java/dp/Leetcode351AndroidUnlockPatterns2.java
@@ -15,54 +15,16 @@
 
 package dp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * <pre>
- *      QuestionEditorial Solution
- * Difficulty: Medium
- *
- *
- * Company Tags Google
- * Tags
- *      Dynamic Programming
- *      Backtracking
- * ===================================================================================
- * ??
- *     1 union find? no, it is DFS
- *     2 for a given resolution, the  all selected key must be connected? Yes
- *     3 The order of keys used matters.
- *     4 why the condition is m ≤ n, a scope, not a number? e.g. n or m? no meaning, the later is better. maybe just for
- *     performance checking
- *     5 how to DP?
- *     //todo
- *
- *          1    2    3
- *
- *          4    5    6
- *
- *          7    8    9
- *
- *  Note:
- *    1-8, 1-6 is direct relation, just like 1-2, 1-5 and 1-4.
- *    DFS idea:
- *      how to define next steps?
- *          may run into problem of circle or circles. while from DFS perspective it looks simple.
- *          next steps also include those keys to which can jumping from current key.
- *    performance:
- *         watch symmetric the keys 1,3,7,9;  2,4,6,8.
- *
- * @see <a href="https://leetcode.com/problems/android-unlock-patterns/">leetcode</a>
- * @see <a href="https://discuss.leetcode.com/topic/46786/sharing-my-bitmask-dynamic-programming-solution-4ms">cpp 0ms DP?? leetcode</a>
- * @see <a href="https://discuss.leetcode.com/topic/46142/share-a-bitmask-dp-solution">cpp 12 ms DP?? leetcode</a>
+ * replace ArrayList selected with boolean[] selected
  */
-public class Leetcode351AndroidUnlockPatterns {
+public class Leetcode351AndroidUnlockPatterns2 {
     private static Map<Integer, Set<Integer>> directTos = new HashMap();
     private static int[][] canJumpTos = new int[10][10];
 
@@ -103,49 +65,53 @@ public class Leetcode351AndroidUnlockPatterns {
         directTos.put(5, new HashSet(Arrays.asList(1, 2, 3, 4, 6, 7, 8, 9)));
     }
 
-    private static Set<Integer> nexts(int cur, List<Integer> selected) {
+    private static Set<Integer> nexts(int cur, boolean[] selected) {
         Set<Integer> r = new HashSet<>();
         r.addAll(directTos.get(cur));
-
-        for (int key : selected) {
-            if (canJumpTos[cur][key] != 0) {
+        for (int key = 1; key <= 9; key++) {
+            if (selected[key]) {
+                r.remove(key);
+            }
+        }
+        for (int key = 1; key <= 9; key++) {
+            if (selected[key] && canJumpTos[cur][key] != 0 && !selected[canJumpTos[cur][key]]) {
                 r.add(canJumpTos[cur][key]);
             }
         }
-        r.removeAll(selected);
+
         return r;
     }
 
-    private static void recursion(int cur, List selected, int sum, int[] counts) {
-        if (selected.size() == sum) {
+    private static void recursion(int cur, boolean[] selected, int remain, int[] counts) {
+        if (remain == 0) {
             counts[0]++;
             return;
         }
         Set<Integer> options = nexts(cur, selected);
         for (int key : options) {
-            selected.add(key);
-            recursion(key, selected, sum, counts);
-            selected.remove(selected.size() - 1);
+            selected[key] = true;
+            recursion(key, selected, remain - 1, counts);
+            selected[key] = false;
         }
     }
 
     private static int waysOf(int keys) {
         int[] sum = new int[1];
-        List<Integer> selected = new ArrayList<>();
+        boolean[] selected = new boolean[10];
 
-        selected.add(1);
-        recursion(1, selected, keys, sum);
-        selected.remove(selected.size() - 1);
+        selected[1] = true;
+        recursion(1, selected, keys - 1, sum);
+        selected[1] = false;
 
-        selected.add(2);
-        recursion(2, selected, keys, sum);
-        selected.remove(selected.size() - 1);
+        selected[2] = true;
+        recursion(2, selected, keys - 1, sum);
+        selected[2] = false;
 
         sum[0] *= 4;
 
-        selected.add(5);
-        recursion(5, selected, keys, sum);
-        selected.remove(selected.size() - 1);
+        selected[5] = true;
+        recursion(5, selected, keys - 1, sum);
+        selected[5] = false;
 
         return sum[0];
     }
---
+++ dataStructuresAndAlgorithms/src/main/java/dp/Leetcode351AndroidUnlockPatterns3.java
@@ -0,0 +1,70 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package dp;
+
+/**
+ * <pre>
+ * DFS
+ * othere idea of deciding next key i:
+ * If key i is not visited && (cur and i are adjacent || not adjacent but the skipped key is already selected)
+ *
+ * what a given 2 keys are adjacent or not is decided there value in skipped array;
+ *
+ * @see <a href="https://discuss.leetcode.com/topic/46260/java-dfs-solution-with-clear-explanations-and-optimization-beats-97-61-12ms">reference</a>
+ */
+public class Leetcode351AndroidUnlockPatterns3 {
+    // remain: the steps remaining
+    int DFS(boolean selected[], int[][] skiped, int cur, int remain) {
+        if (remain < 0) return 0;
+        if (remain == 0) return 1;
+        selected[cur] = true;
+        int rst = 0;
+        for (int i = 1; i <= 9; ++i) {
+            if (!selected[i] && (skiped[cur][i] == 0 || (selected[skiped[cur][i]]))) {
+                rst += DFS(selected, skiped, i, remain - 1);
+            }
+        }
+        selected[cur] = false;
+        return rst;
+    }
+
+    public int numberOfPatterns2(int m, int n) {
+        /**
+         *   adjacent or not
+         *    1    2    3
+         *    4    5    6
+         *    7    8    9
+         */
+        int skiped[][] = new int[10][10];
+        skiped[1][3] = skiped[3][1] = 2;
+        skiped[1][7] = skiped[7][1] = 4;
+        skiped[3][9] = skiped[9][3] = 6;
+        skiped[7][9] = skiped[9][7] = 8;
+        skiped[1][9] = skiped[9][1]
+                = skiped[2][8] = skiped[8][2]
+                = skiped[3][7] = skiped[7][3]
+                = skiped[4][6] = skiped[6][4] = 5;
+        boolean selected[] = new boolean[10];
+        int rst = 0;
+
+        for (int i = m; i <= n; ++i) {
+            rst += DFS(selected, skiped, 1, i - 1) * 4;
+            rst += DFS(selected, skiped, 2, i - 1) * 4;
+            rst += DFS(selected, skiped, 5, i - 1);
+        }
+        return rst;
+    }
+}
