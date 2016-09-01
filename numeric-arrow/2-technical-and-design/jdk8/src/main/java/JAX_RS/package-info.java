006bee7a17f7264d63fca6c33bb4926a4da99953
Bruce Zu <bruce.zu.run10@gmail.com>
2016-08-31 22:40:26 -0700

    Java EE JAX-RS
    Change-Id: I23600d7092f7bc61aa3a6f76c1ebc5e471ce248a

---
+++ numeric-arrow/2-technical-and-design/jdk8/src/main/java/JAX_RS/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * JAX-RS: Java API for RESTful Web Services (JAX-RS)
+ * <pre>
+ * It is a Java programming language API spec
+ * that provides support in creating web services according to the
+ * Representational State Transfer (REST) architectural pattern.[1] JAX-RS uses annotations,
+ * introduced in Java SE 5, to simplify the development and deployment of web service clients and endpoints.
+ *
+ * JAX-RS 2.0
+ * In May 2013, it reached the Final Release stage.
+ *
+ *      Jetty (HTTP)
+ *      Jersey (JAX-RS)
+ *      Jackson (JSON or XML)
+ *      Guava (excellent additions to JDK libraries)
+ *      Metrics (real time application monitoring)
+ *      Hibernate Validator (input verification)
+ *      OAuth (RESTful authentication)
+ *
+ * @see <a href="http://programmers.stackexchange.com/questions/155467/selecting-a-jax-rs-implementation-for-a-new-project">
+ * Selecting a JAX-RS implementation for a new project
+ * </a>
+ * @see <a href="https://en.wikipedia.org/wiki/Java_API_for_RESTful_Web_Services#JAX-RS_2.0">JAX-RS_2.0</a>
+ * @see <a href="https://en.wikipedia.org/wiki/HATEOAS">HATEOAS</a>
+ */
+package JAX_RS;
