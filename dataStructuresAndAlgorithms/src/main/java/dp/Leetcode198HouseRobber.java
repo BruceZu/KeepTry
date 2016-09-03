9fef9c7766dadb73eeeeaf46ed9360e9f90a528a
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-03 10:17:39 -0700

    Leetcode 198 House Robber
    Change-Id: I87efbc796d238f486e85aee0f96732e886f0c223

---
+++ dataStructuresAndAlgorithms/src/main/java/dp/Leetcode198HouseRobber.java
@@ -0,0 +1,41 @@
+//  Copyright 2016 The Sawdust Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package dp;
+
+/**
+ * <pre>
+ *
+ * @see <a href="https://leetcode.com/problems/house-robber/">leetcode</a>
+ */
+public class Leetcode198HouseRobber {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] max = new int[nums.length];
+        max[0] = nums[0];
+        max[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            max[i] = Math.max(max[i - 1], max[i - 2] + nums[i]);
+        }
+        return max[nums.length - 1];
+    }
+    // improve it to space O(1)
+}
