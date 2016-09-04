6736a4e7707bb3a018e7ef77122ce0a28a37f51d
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-03 17:21:38 -0700

    Polish Leetcode 26 Remove Duplicates from Sorted Array
    Change-Id: Ia71137fffbf336c880df831a022d0428bbd90951

--- dataStructuresAndAlgorithms/src/main/java/array/doublepoint/Leetcode26RemoveDuplicatesfromSortedArray.java
+++ dataStructuresAndAlgorithms/src/main/java/array/doublepoint/Leetcode26RemoveDuplicatesfromSortedArray.java
@@ -16,29 +16,21 @@
 package array.doublepoint;
 
 /**
- * Difficulty: Easy <pre>
- * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
  * Do not allocate extra space for another array, you must do this in place with constant memory.
- * For example,
- * Given input array nums = [1,1,2],
- * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
- * It doesn't matter what you leave beyond the new length.
- * Subscribe to see which companies asked this question
- * Hide Tags Array Two Pointers
- * Hide Similar Problems (E) Remove Element
+ *
+ * Array
+ * Two Pointers
+ *
  * @see <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/">leetcode</a>
  */
 public class Leetcode26RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int nextDistinguishIndex = 1;
+        int size = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[nextDistinguishIndex - 1]) {
-                nums[nextDistinguishIndex++] = nums[i];
+            if (nums[i] != nums[i - 1]) {
+                nums[size++] = nums[i];
             }
         }
-        return nextDistinguishIndex;
+        return size;
     }
 }
