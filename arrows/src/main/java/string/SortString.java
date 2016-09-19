58adb1dcc96ab7d475dc54c45d35e8ee5261171b
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-19 14:26:06 -0700

    Sort String
    Change-Id: I9f1ee3c4bd80894a7f4997ad3bb81a3f95274f20

---
+++ arrows/src/main/java/string/SortString.java
@@ -0,0 +1,62 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UnknownFormatConversionException;
+
+/**
+ * how about if there is supplementary character
+ * how about there is invalid supplementary character.
+ */
+public class SortString {
+
+    public String sort(String inputString, String sortOrder) throws UnknownFormatConversionException {
+        // corner case check
+        if (inputString == null || inputString.length() == 0) {
+            return "";
+        }
+        Map<String, Integer> times = new HashMap();
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            if (Character.isSurrogate(c)) {
+                if (Character.isHighSurrogate(c)
+                        && i + 1 < inputString.length() && Character.isLowSurrogate(inputString.charAt(i + 1))) {
+                    String key = c + "" + inputString.charAt(i + 1);
+                    Integer n = times.get(key);
+                    times.put(key, n == null ? 1 : n + 1);
+                    i++; // care
+
+                } else {
+                    throw new UnknownFormatConversionException("invalid supplementary character in " + inputString);
+                }
+            } else {
+                Integer n = times.get(c);
+                times.put("" + c, n == null ? 1 : n + 1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sortOrder.length(); i++) {
+            char c = sortOrder.charAt(i);
+            String key;
+            if (Character.isSurrogate(c)) {
+                if (Character.isHighSurrogate(c)
+                        && i + 1 < sortOrder.length() && Character.isLowSurrogate(sortOrder.charAt(i + 1))) {
+                    key = c + "" + sortOrder.charAt(i + 1);
+                    i++; // care
+                } else {
+                    throw new UnknownFormatConversionException("invalid supplementary character in " + sortOrder);
+                }
+            } else {
+                key = "" + c;
+            }
+
+            if (times.containsKey(key)) {
+                int t = times.get(key);
+                while (t-- != 0) {
+                    sb.append(key);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
