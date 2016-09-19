4a543d82073ac296c0aae4d423c4698424ac68c6
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-18 19:32:16 -0700

    Double NaN
    Change-Id: Ia208235f55559684094167032105b07f54ab39e5

--- bow/2-technical-and-design/jdk8/src/main/java/java_double/Java_Double.java
+++ bow/2-technical-and-design/jdk8/src/main/java/java_double/Java_Double.java
@@ -6,6 +6,27 @@ public class Java_Double {
         // Double.MIN_VALUE is positive, > 0
         // why it is so designed?
         System.out.println(Math.min(Double.MIN_VALUE, 0.0d));
-    }
 
+        //doing floating point arithmetic they don't pay enough attention to
+        // Double.INFINITY,
+        // NaN,
+        // -0.0
+        // and other rules that govern the arithmetic calculations involving them.
+        System.out.println(1.0 / 0.0); // Infinity
+        System.out.println(-1.0 / 0.0); // -Infinity
+        System.out.println(-0.0);  // -0.0
+        System.out.println(0.0 / 0.0); // NaN
+
+
+        // x == Double.NaN always evaluates to false,
+        // even if x itself is a NaN. To test if x is a NaN,
+        // one should use the method call Double.isNaN(x) to check if given number is NaN or not.
+        //
+        // If you know SQL, this is very close to NULL there.
+
+        System.out.println(1 == Double.NaN);
+        System.out.println(Double.NaN == Double.NaN);
+        System.out.println(Double.isNaN(0));
+        System.out.println(Double.isNaN(Double.NaN));
+    }
 }
