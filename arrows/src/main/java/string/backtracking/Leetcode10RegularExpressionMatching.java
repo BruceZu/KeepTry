cf0958c145ec83ff6c3cfcab2fba076d27e376b2
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-19 20:00:14 -0700

    Polish
    Change-Id: I3faeaf68a49050ba2740e8775b51da788d368e60

--- arrows/src/main/java/string/backtracking/Leetcode10RegularExpressionMatching.java
+++ arrows/src/main/java/string/backtracking/Leetcode10RegularExpressionMatching.java
@@ -16,14 +16,16 @@
 package string.backtracking;
 
 /**
- * Difficulty: Hard
- * Implement regular expression matching with support for '.' and '*'.
  * <pre>
- * '.' Matches any single character.
- * '*' Matches zero or more of the preceding element.
+ * Difficulty: Hard
+ *
+ * Implement regular expression matching with support for '.' and '*'.
+ *
+ *   '.' Matches any single character.
+ *   '*' Matches zero or more of the preceding element.
+ *
  * The matching should cover the entire input string (not partial).
- * The function prototype should be:
- * bool isMatch(const char *s, const char *p)
+ *
  * Some examples:
  * isMatch("aa","a") → false
  * isMatch("aa","aa") → true
@@ -34,60 +36,87 @@ package string.backtracking;
  * isMatch("aab", "c*a*b") → true
  *
  * Tags: Dynamic Programming, Backtracking, String
+ * ===========================================================
+ * TDD
+ * s= "aa", p="a"
+ * s= "a",  p="ab*"
+ * s= "ab", p= ".*c"
+ * s= "a",  p= "ab*"
+ * s= "aa", p= "a*" or "a*aa"
+ * s= "aaa",  p= "a*a"
+ * s= "",     p= ".*"
+ * s= "aaab", p="a*b"
+ *
+ *   1 before compare current char of each other.
+ *    <strong>firstly checking the next char</strong> in 'p' to see if it is exist and it is *
+ *           is so:
+ *              check if it is representable:
+ *                        .*
+ *                        or
+ *                        both current chars in 'p' and 's' are same
+ *                  if so: means backtracking all possible:
+ *                      for .* :  represent 0, 1, ... till the last member.
+ *                      for [any char]*  represent 0, 1, ... till the last [any char].
+ *                  after try all possible, if still failed. e.g.
+ *                  //  aaab,  a*b
+ *                         |     |
+ *                  try next one after [any char] in s. by pi+2 in p.
+ *             else no representable, take the * in .* or [any char]* as zero.
+ *                 e.g. // cba, a*.ba
+ *  2  else check current char is '.' || current chars are same
+ *            then continue
+ *  3  else false;
+ *
+ *
+ * Note: bounder check
+ * Follow up:
+ *
+ *  the given string contains '.' or '*' ?
+ *  can the '*'  be used independently?
+ *
+ *  cache the backtracking parts
+ *
+ * @see <a href="https://leetcode.com/problems/regular-expression-matching/">leetcode</a>
  */
 
 public class Leetcode10RegularExpressionMatching {
-}
 
-class Solution {
-    /**
-     * <pre>
-     * need back-tracing when it is
-     * ".*" or  x*.
-     *          |
-     *         a char same as the given char
-     *
-     * @param s
-     * @param is current index of string s.
-     * @param p
-     * @param ip current index of pattern p.
-     * @return
-     */
-    public boolean byBackTracing(char[] s, int is, char[] p, int ip) {
-        if (cache[is][ip] != null) {
-            return cache[is][ip];
-        }
-        // index valid check
-        if (is == s.length && ip == p.length) { // both end
+    private static boolean backtracking2(char[] s, int si, char[] p, final int pi) {
+        if (si == s.length && pi == p.length) {
             return true;
         }
 
-        if (ip + 1 < p.length && p[ip + 1] == '*') {
-            while (ip < p.length && is < s.length
-                    && p[ip + 1] == '*' && p[ip] == '.' ? true : is < s.length && p[ip] == s[is]) {
-                // back-tracing and cache
-                if (cache[is][ip + 2] = byBackTracing(s, is, p, ip + 2)) {
+        if (pi + 1 < p.length && p[pi + 1] == '*') {
+            final int nextPIndex = pi + 2;
+            while (si < s.length && (p[pi] == '.' || p[pi] == s[si])) {
+                if (backtracking2(s, si, p, nextPIndex)) {
                     return true;
+                } else {
+                    si++; //care
                 }
-                is++;
             }
-            return byBackTracing(s, is, p, ip + 2); // take it as no existing, * is zero
+            // all possible represent (== relation) is tried
+            return backtracking2(s, si, p, pi + 2); // care
 
-        } else if (ip < p.length && p[ip] == '.'
-                || ip < p.length && is < s.length && p[ip] == s[is]) {
-            return byBackTracing(s, is + 1, p, ip + 1);
+        } else if (pi < p.length && p[pi] == '.'
+                || pi < p.length && si < s.length && p[pi] == s[si]) {
+            return backtracking2(s, si + 1, p, pi + 1);
+        } else {
+            return false;
         }
-
-        return false;
     }
 
-    Boolean[][] cache;
-
-    public boolean isMatch(String s, String p) {
-        if (p.equals(s) || p.equals(".*")) {
+    public static boolean isMatch(String s, String p) {
+        if (s == null) {
+            return false;
+        }
+        if (s.equals(p) || p == ".*") {
             return true;
         }
-        cache = new Boolean[s.length() + 3][p.length() + 3];
-        return byBackTracing(s.toCharArray(), 0, p.toCharArray(), 0);
+        return backtracking2(s.toCharArray(), 0, p.toCharArray(), 0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aaab", "a*b"));
     }
 }
