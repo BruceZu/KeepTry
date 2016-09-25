033fe1ed7a551f43ccc3a9893432dd7449b9f81a
Bruce Zu <bruce.zu.run10@gmail.com>
2016-09-24 21:16:09 -0700

    Update iterator binary tree using stack
    Change-Id: I51cd468d3949f67fab54fa46b79a209b361b29cf

--- arrows/src/main/java/treesimple/binarytree/TraversalBTusingStack.java
+++ arrows/src/main/java/treesimple/binarytree/TraversalBTusingStack.java
@@ -31,53 +31,89 @@ public class TraversalBTusingStack {
     }
 
     /**
+     * Data structure:
+     * Stack
+     * what is the stack for?
+     * keep sub trees
+     *
      * @see <a href="http://www.jiuzhang.com/solutions/binary-tree-preorder-traversal/">jiuzhang</a>
      */
     public static void printPreOrder(TreeNode n) {
         if (n == null) {
             return;
         }
-        Stack<TreeNode> s = new Stack();
-
-        System.out.println(n.v);
-        s.push(n);
-        while (!s.empty()) {
-            if (n.left != null) {
-                n = n.left;
-                System.out.println(n.v);
-                s.push(n);
-                continue;
-            }
+        Stack<TreeNode> subtrees = new Stack();
+        subtrees.push(n);
+        while (!subtrees.empty()) {
+            n = subtrees.pop();
+            System.out.println(n.v);
             if (n.right != null) {
-                n = n.right;
-                System.out.println(n.v);
-                s.push(n);
-                continue;
+                subtrees.push(n.right);
             }
-            TreeNode p;
-            while (true) {
-                n = s.pop();
-                if (s.empty()) {
-                    break  ;
-                }
-                p = s.peek();
-                if (n == p.left && p.right != null) {
-                    n = p.right;
-                    System.out.println(n.v);
-                    s.push(n);
-                    break;
-                }
+            if (n.left != null) {
+                subtrees.push(n.left);
             }
         }
     }
 
     /**
-     * @see <a href="http://www.jiuzhang.com/solutions/binary-tree-postorder-traversal/">jiuzhang</a>
+     * 下黄泉,上穷碧落<pre>
+     * step 1: down to bottom without not breaking where is the start place
+     * step 2: find bottom, process it
+     * step 3: next one:
+     *          according to post order:
+     *          if bottom have parent:
+     *               if bottom is right child, continue up
+     *               if bottom is left child and no brother, continue up
+     *               if bottom is left child and have right brother. then go to brother.
+     *          else: end up
+     * <p>
+     * what is stack for?
+     *   keep track of parents only.
+     *   @see <a href="http://www.jiuzhang.com/solutions/binary-tree-postorder-traversal/">jiuzhang</a>
      */
     public static void printPostOrder(TreeNode n) {
         if (n == null) {
             return;
         }
+        Stack<TreeNode> parents = new Stack();
+        while (true) {
+            // 1
+            parents.push(n);
+            while (true) {
+                if (n.left != null) {
+                    n = n.left;
+                } else if (n.right != null) {
+                    n = n.right;
+                } else {
+                    break; //n.left == null &&  n.right == null
+                }
+                parents.push(n);
+            }
+            // 2
+            parents.pop();
+            System.out.println(n.v);
+
+            // 3
+            while (!parents.empty() &&
+                    (n == parents.peek().right
+                            || n == parents.peek().left && parents.peek().right == null
+                    )) {
+                n = parents.pop();
+                System.out.println(n.v);
+            }
+
+            if (parents.empty()) {
+                break;
+            }
+            n = parents.peek().right;
+        }
+    }
+
+    public static void printPostOrder2(TreeNode n) {
+        if (n == null) {
+            return;
+        }
         Stack<TreeNode> s = new Stack();
         loop:
         while (true) {
@@ -110,48 +146,55 @@ public class TraversalBTusingStack {
     }
 
     /**
-     * @see <a href="http://www.jiuzhang.com/solutions/binary-tree-inorder-traversal/">jiuzhang</a>
+     * <pre>
+     * Special:  It is useful for Binary search tree
+     *
+     * how to check all case of BT:
+     *     use the fish head
+     *
+     * Data Structure:
+     *   stack and current Node pointer <Strong>which is not got from the top of stack</Strong>.
+     *
+     * How to handle the stack?
+     *   easy push in, hard to handle when and how to pop out.
+     *   Firstly need know the meanings of stack for.
+     * what is the stack for?
+     *    save left branches.
+     *    keep track from top to down of left branch
+     *    till the end where left sub tree is null.
+     *    then according to in order print or process the current node,
+     *    then its right subtree if the right subtree is not null
+     *
+     *    Any tree can be understand as composited by left branches,
+     *    sometimes the left branch is only one node.
+     *
+     * How to handle stack:
+     *    1 step   track left branch from top to down till the end
+     *    2 step   it is branch end, so back up and process the end one
+     *             here need check if end up all
+     *    3 step   see if continue down to sub right tree, continue track left branch from there
      */
     public static void printInOrder(TreeNode n) {
-        if (n == null) {
-            return;
-        }
-        Stack<TreeNode> s = new Stack();
-        loop:
+        Stack<TreeNode> leftBranches = new Stack();
         while (true) {
-            if (n.left != null) {
-                s.push(n);
+            while (n != null) {//1
+                leftBranches.push(n);
                 n = n.left;
-                continue;
-            }
-            System.out.println(n.v);
-            if (n.right != null) {
-                s.push(n);
-                n = n.right;
-                continue;
             }
 
-            TreeNode p;
-            while (true) {
-                p = s.peek();
-                if (p.left == n) {
-                    System.out.println(p.v);
-                }
-                if (n == p.left && p.right != null) {
-                    n = p.right;
-                    break;
-                }
-                n = s.pop();
-                if (s.empty()) { // n is root
-                    break loop;
-                }
+            if (leftBranches.empty()) {//2
+                break;// end up all
             }
+            n = leftBranches.pop();
+            System.out.println(n.v);
+
+            n = n.right;//3
         }
     }
 
     public static void main(String[] args) {
         /**
-         * <pre> pre-order
+         * <pre> pre-order: top is first
          *        1
          *      /  \
          *     2   6
@@ -175,7 +218,7 @@ public class TraversalBTusingStack {
         printPreOrder(r);
 
         /**
-         * <pre> post-order
+         * <pre> post-order : down is first
          *        9
          *      /  \
          *     4   8
@@ -199,7 +242,7 @@ public class TraversalBTusingStack {
         printPostOrder(r);
 
         /**
-         * <pre> in-order
+         * <pre> in-order: left first, binary search tree
          *        5
          *      /  \
          *     4   6
