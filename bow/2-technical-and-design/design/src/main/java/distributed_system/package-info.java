78d3f9b601b4b8a762698d9349850ea01e8960ff
Bruce Zu <bruce.zu.run10@gmail.com>
2016-10-01 18:22:52 -0700

    distributed system
    Change-Id: I42f5c76dbdb53ec51d43e47b3d82d25c61162554

--- bow/2-technical-and-design/design/src/main/java/distributed_system/package-info.java
+++ bow/2-technical-and-design/design/src/main/java/distributed_system/package-info.java
@@ -1,1 +1,18 @@
+/**
+ * <pre>
+ *
+ * Zookeeper:
+ * Chapter 14 of <<Hadoop: The Definitive Guide>>
+ * which has ~35 pages explaining essentially what ZooKeeper does,
+ * followed by an example of a configuration service.
+ *
+ * Zookeeper is a CP system (Refer CAP Theorem) that provides Consistency and Partition tolerance.
+ * Replication of Zookeeper state across all the nods makes it an eventually consistent distributed service.
+ *
+ * any newly elected leader will updates its followers with missing proposals or with a snap shot of the state,
+ * if the followers has many proposals missing.
+ * <a href="http://stackoverflow.com/questions/3662995/explaining-apache-zookeeper">zookeeper</a>
+ * <hi><a href="https://www.quora.com/What-is-the-actual-role-of-ZooKeeper-in-Kafka">ZooKeeper-in-Kafka</a>
+ * <hi><a href="http://massivetechinterview.blogspot.com/2015/08/kafka-internal.html">kafka-internal</a>
+ */
 package distributed_system;
