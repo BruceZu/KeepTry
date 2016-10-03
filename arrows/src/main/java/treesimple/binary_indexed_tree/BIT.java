35ae428ad1a064c26cd31b5da174b6f5d8aaca1e
Bruce Zu <bruce.zu.run10@gmail.com>
2016-10-02 22:31:37 -0700

    binary indexed tree
    Change-Id: Ie9e2e19c540f87fc33b305e5a2a08db7b98c6ae3

---
+++ arrows/src/main/java/treesimple/binary_indexed_tree/BIT.java
@@ -0,0 +1,17 @@
+package treesimple.binary_indexed_tree;
+
+public class BIT {
+    private int[] BIT;
+
+    public void build(int[] origin) {
+
+    }
+
+    public void add(int index, int v) {
+
+    }
+
+    public int prefixsum(int to) {
+
+    }
+}
