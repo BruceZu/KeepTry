d25b8f9ce1a146a3dca13d3bf7d45050b1459b74
Bruce Zu <bruce.zu.run10@gmail.com>
2016-10-19 21:44:09 -0700

    Polish
    Change-Id: I7d7056d30fb957f789ea3139914456b8d9ef5fb4

--- arrows/src/main/java/dp/Leetcode10RegularExpressionMatching2.java
+++ arrows/src/main/java/dp/Leetcode10RegularExpressionMatching2.java
@@ -5,13 +5,14 @@ package dp;
  * <p>
  * the first time of 2D not always be false.
  * dp start from left to right.
+ * <img src="../../resources/RegularExpression.png" height="400" width="600">
  *
  * @see <a href="https://www.youtube.com/watch?v=l3hda49XcDE">youtube</a>
  * @see <a href="https://leetcode.com/problems/regular-expression-matching/">leetcode</a>
  */
 public class Leetcode10RegularExpressionMatching2 {
     // 27 ms, beat 46%
-    public boolean isMatch(String s, String p) {
+    public static boolean isMatch(String s, String p) {
         if (s == null || p == null) {
             return false;
         }
@@ -20,32 +21,41 @@ public class Leetcode10RegularExpressionMatching2 {
         boolean T[][] = new boolean[str.length + 1][pattern.length + 1];
 
         T[0][0] = true;
+
         //Deals with patterns like a* or a*b* or a*b*c*
-        for (int i = 1; i < T[0].length; i++) {
-            if (pattern[i - 1] == '*') {
-                T[0][i] = T[0][i - 2];
+        for (int tColum = 1; tColum < T[0].length; tColum++) {
+            int pi = tColum - 1;
+            if (pattern[pi] == '*') {
+                T[0][tColum] = T[0][tColum - 2];
             }
         }
 
         char curP, preP, curS;
-        for (int i = 1; i < T.length; i++) {
-            for (int j = 1; j < T[0].length; j++) {
-                curP = pattern[j - 1];
-                curS = str[i - 1];
+        for (int stri = 1; stri < T.length; stri++) {
+            for (int pi = 1; pi < T[0].length; pi++) {
+                curP = pattern[pi - 1];
+                curS = str[stri - 1];
                 if (curP == '.' || curP == curS) {
-                    T[i][j] = T[i - 1][j - 1];
+                    T[stri][pi] = T[stri - 1][pi - 1];
                 } else if (curP == '*') {
-                    preP = pattern[j - 2];
-                    T[i][j] = T[i][j - 2]; // * is 0
+                    preP = pattern[pi - 2];
+                    T[stri][pi] = T[stri][pi - 2]; // * is 0  // move left 2 step
                     if (preP == '.' || preP == curS) {
                         // T[i][j] = T[i][j-2] || T[i][j - 1] || T[i - 1][j]; // * is 0,1,more than 1.
-                        T[i][j] |= T[i - 1][j]; // * is 0,1,more than 1.
+                        T[stri][pi] |= T[stri - 1][pi]; // * is 0,1,more than 1. // move up 1 step
                     }
                 } else {
-                    T[i][j] = false;
+                    T[stri][pi] = false;
                 }
             }
         }
         return T[str.length][pattern.length];
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(isMatch("", "a*b*c*"));
+
+
     }
 }
