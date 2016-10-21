a26053b775687a7be6bdc90ef2804c847e2a87a5
Bruce Zu <bruce.zu.run10@gmail.com>
2016-10-21 16:27:11 -0700

    search substring
    Change-Id: Ie3ddd832b481716f77c974d8ceb8b4dbc2e7213f

--- arrows/src/main/java/string/Boyer_Moore_string_search_algorithm/SearchSubStr.java
+++
@@ -1,8 +0,0 @@
-package string.Boyer_Moore_string_search_algorithm;
-
-/***
- * //todo:
- * <br><a href= "https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_string_search_algorithm">Boyer–Moore string search algorithm </a>
- */
-public class SearchSubStr {
-}
