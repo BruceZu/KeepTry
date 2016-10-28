8c64acea7cc5c70b42ba89530c6178a0be6d1388
Bruce Zu <bruce.zu.run10@gmail.com>
2016-10-27 22:18:52 -0700

    make copyright
    Change-Id: I6e178064226cb5799dad551934508ead4f2e8128

--- arrows/src/main/java/subset/C623SubsetWithStackAndQueue.java
+++ arrows/src/main/java/subset/C623SubsetWithStackAndQueue.java
@@ -1,15 +1,15 @@
-//  Copyright 2016 The stackawdust Open stackource Project
+//  Copyright 2016 The Sawdust Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENstackE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "Astack Istack" BAstackIstack,
-// WITHOUT WARRANTIEstack OR CONDITIONstack OF ANY KIND, either express or implied.
-// stackee the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 //
 
