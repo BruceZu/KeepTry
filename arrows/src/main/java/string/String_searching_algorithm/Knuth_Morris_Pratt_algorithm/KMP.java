7b7556517f97b4c75c42fb7684e374dad9ed00bb
Bruce Zu <bruce.zu.run10@gmail.com>
2017-02-07 20:04:16 -0800

    Polish KMP

--- arrows/src/main/java/string/String_searching_algorithm/Knuth_Morris_Pratt_algorithm/KMP.java
+++ arrows/src/main/java/string/String_searching_algorithm/Knuth_Morris_Pratt_algorithm/KMP.java
@@ -15,222 +15,132 @@
 
 package string.String_searching_algorithm.Knuth_Morris_Pratt_algorithm;
 
-import com.sun.org.apache.regexp.internal.RE;
-
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * <a href ="https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm">wiki</a>
  * <pre>
- *   The most straightforward algorithm
- *   s[] length is K
- *   W[] length is n
- *    worst-case performance is O(k⋅n)
- *
+ *   worst-case performance is O(k⋅n)
+ *   s[] length is n
+ *   W[] length is k
  *   e.g.    s[] =  AAAAAA
  *           W[] =  AAB
  */
 public class KMP {
-    private static HashMap<String, int[]> strToPMT = new HashMap();
-
     /**
      * <pre>
-     *  Objective is NOT move to the next same character as the first one.
-     *  when there are more common parts, each of then have different length, select the one with max length
-     *
-     *  KMP spends a little time precomputing a table (on the order of the size of W[], O(n)),
-     *  and then it uses that table to do an efficient search of the string in O(k).
-     *  PMT indicates where we need to look for the start of a new match in the event that the current one ends in a mismatch.
-     *  <img src="../../../../resources/sub_string_KMP.png" height="450" width="300">
-     *  Note here the arr related str is the one before the no match index of w.
-     *  It is [0, not match index i -1]
-     *  so when the w[0] is not match. moveSteps will be 1. i will be set to be 0;
-     *  This is special case. precess it before call PMT
-     */
-    static int[] partialMatchTable(char[] arr) {
-        if (arr == null || arr.length == 0) {
-            throw new IllegalArgumentException("Assume the pattern str is not null and empty");
-        }
-        int[] v = new int[arr.length];
-        v[0] = 0;
-
-        for (int vIndex = 1; vIndex < arr.length; vIndex++) {//vIndex: index before the not match index
-            // longer substring first
-            for (int subStrStartIndex = 1; subStrStartIndex <= vIndex; subStrStartIndex++) {
-                // filter by both side chars
-                if (arr[subStrStartIndex] == arr[0] && arr[vIndex] == arr[vIndex - subStrStartIndex]) {
-                    // do not check middles chars, for the case arr = "AA", no middle part.
-                    int j = subStrStartIndex;
-                    while (j <= vIndex) {
-                        if (arr[j] != arr[j - subStrStartIndex]) {
-                            break;
-                        }
-                        j++;
-                    }
-                    // if current sub string match, do not check the following shorter substring
-                    if (j == vIndex + 1) {
-                        v[vIndex] = vIndex - subStrStartIndex + 1;
-                        break;
-                    }
-                }
-            }
-        }
-        return v;
-    }
-
-    /**
-     * <pre>
-     * "Partial match" table (also known as "failure function")
-     * The table determines how much KMP will increase (variable m) and where it will resume testing (variable i).
-     * The goal of the table is to allow the algorithm not to match any character of S more than once. 
-     * The complexity of the table algorithm is O(k), where k is the length of W
-     *
-     * v of T[i]: the length of the longest prefix start with the w[0], which is also the suffix end with w[current index i]
-     *
-     * if can renew T[i-1], then renew.
-     * else if i-1 处 次最长prefix, also the suffix, exist (its length !=0 ), try to renew
-     * else (its length ==0) compare directly with W[0]
+     * get Partial Match Table of pattern w
+     * assume w length is at least 1.
+     * assume w does not contain supplementary character
      * <img src="../../../../resources/KMP_partial_T.png" height="450" width="500">
-     * @param w
-     * @return
+     * O(k), where k is the length of pattern W
+     *
+     *
+     * "Partial match" table (also known as "failure function")
+     * when mismatch happen The PMT determines how much KMP will increase (variable si) and
+     * where it will resume testing (variable wi). The goal of the table is to allow the algorithm not to
+     * match any character of S more than once. 
+     *
+     * meaning of value of w[i] is in the array start with index 0 and end up with index of i (including)
+     * the length of partial_match(i) which is the longest prefix start with the w[0],
+     * it is also the length of the longest suffix end up with w[i] (including).
+     *
+     * Use of "Partial match" table:
+     * get the value of "Partial match" table of pattern 'W' at index of 'indexBeforeNotMatch':
+     * PMT[indexBeforeNotMatch]
+     * where 'indexBeforeNotMatch' should not be -1:
+     * when the w[0] is not match. moveSteps will be 1.
+     * This is special case. precess it before using PMT
      */
+    static int[] getPartialMatchTable(String pattern) {
+        char[] w = pattern.toCharArray();
+        int[] PMT = new int[w.length];
 
-    static int[] partialMatchTable2(char[] w) {
-        int[] T = new int[w.length];
-        T[0] = 0;
-        int indexCompareTo = 0;
+        PMT[0] = 0;
         for (int i = 1; i < w.length; i++) {
+            int indexTryToRenew = PMT[i - 1]; // length of partial_match(i-1)
             while (true) {
-                if (indexCompareTo == 0) {
-                    if (w[i] == w[0]) {
-                        T[i] = 1;
-                        indexCompareTo++;
-                    } else {
-                        T[i] = 0;
-                    }
+                if (w[i] == w[indexTryToRenew]) { // try renew
+                    PMT[i] = indexTryToRenew + 1;
                     break;
                 }
-
-                if (w[indexCompareTo] == w[i]) {
-                    T[i] = indexCompareTo + 1;
-                    indexCompareTo++;
+                if (indexTryToRenew == 0) { // can not go back
+                    PMT[i] = 0;
                     break;
                 }
-                indexCompareTo = T[indexCompareTo - 1]; // Not: use indexCompareTo, not i
+                indexTryToRenew = PMT[indexTryToRenew - 1];
             }
         }
-        return T;
+        return PMT;
     }
 
-    static int valueOfPMT(String pattern, int indexBeforeNotMatch) {
-        if (indexBeforeNotMatch == -1) {
-            throw new IllegalArgumentException("so when the w[0] is not match. moveSteps will be 1. i will be set to be 0;   " +
-                    " This is special case. precess it before call PMT");
-        }
-        int[] pmt = strToPMT.get(pattern);
-        if (pmt == null) {
-            strToPMT.put(pattern, partialMatchTable2(pattern.toCharArray()));
-        }
-        return strToPMT.get(pattern)[indexBeforeNotMatch];
-    }
 
-    static int moveSteps(String pattern, int notMatchIndex) {
-        return notMatchIndex - valueOfPMT(pattern, notMatchIndex - 1);
-    }
-
-    //  O(n), where n is the length of S
+    // get the index of s where find the first match of give w string.
+    // O(n), where n is the length of S
     static int KMP(String s, String w) {
         if (s == null || w == null) {
             throw new IllegalArgumentException("Assume the S and W str is not null");
         }
         if (w == "") {
             return 0;
-            // reference:
-            //   String.indexOf(char[] source, int sourceOffset, int sourceCount,
-            //                  char[] target, int targetOffset, int targetCount,
-            //                  int fromIndex) {
-            //
-            //
-            // if (targetCount == 0) {
-            //     return fromIndex;
-            // }
         }
-        //m, the beginning index of the current match in S
-        //i, the index of the current character in W
-        int m = 0;
-        int i = 0;
-        while (true) { //the loop executes at most 2n times
-            // -------------------
-//            System.out.print("\n\n");
-//            for (int index = 0; index < s.length(); index++) {
-//                System.out.print(index % 10);
-//            }
-//            System.out.println("\n" + s);
-//            for (int space = 0; space < m; space++) {
-//                System.out.print(" ");
-//            }
-//            System.out.println(w);
-//            for (int space = 0; space < m; space++) {
-//                System.out.print(" ");
-//            }
-//            for (int index = 0; index < w.length(); index++) {
-//                System.out.print(index % 10);
-//            }
-//            System.out.println();
-            // -------------------
-            if (i == w.length()) { // match completely
-                return m;
+
+        int[] PMT = getPartialMatchTable(w);
+        int startInS = 0; // in S, the beginning index to compare with W
+        int wi = 0; // in W, the index of the current character
+        while (true) { // the loop executes at most 2 * S.length() times
+            if (wi == w.length()) { // match completely
+                return startInS;
             }
-            if (m + i == s.length()) {
+            if (startInS + wi == s.length()) {
                 return -1; // not found
             }
-
-            if (s.charAt(m + i) == w.charAt(i)) {
-                // System.out.println(String.format("w %d match s %d", i, m + i));
-                i++;
-            } else {
-                //System.out.println(String.format("w %d Does Not match s %d", i, m + i));
-                int value;
-                int moveSteps;
-                if (i == 0) {
-                    // when the w[0] is not match. moveStep will be 1. i =0
-                    m++;
-                    //System.out.println("Move steps " + 1);
+            // continue compare
+            if (s.charAt(startInS + wi) == w.charAt(wi)) {
+                wi++;
+            } else { // Does Not match, need move
+                if (wi == 0) { // do not use "Partial match" table
+                    startInS++;
                 } else {
-                    value = valueOfPMT(w, i - 1);
-                    moveSteps = i - value;
+                    int indexCompareFromW = PMT[wi - 1];
+                    int moveSteps = wi - indexCompareFromW;
 
-                    i = valueOfPMT(w, i - 1);
-                    m += moveSteps;
-                    //System.out.println("Move steps " + moveSteps);
+                    wi = indexCompareFromW;
+                    startInS += moveSteps;
                 }
             }
         }
     }
 
+    // -----------------------------------------------------------------------------------------------------
+    static int moveSteps(String w, int notMatchIndex) {
+        return notMatchIndex - getPartialMatchTable(w)[notMatchIndex - 1];
+    }
+
     public static void main(String[] args) {
         // PMT
-        System.out.println(Arrays.toString(partialMatchTable("A".toCharArray())));
-        System.out.println(Arrays.toString(partialMatchTable("AA".toCharArray())));
-        System.out.println(Arrays.toString(partialMatchTable("AB".toCharArray())));
-        System.out.println(Arrays.toString(partialMatchTable("ABCAB".toCharArray())));
+        System.out.println(Arrays.toString(getPartialMatchTable("A"))); // [0]
+        System.out.println(Arrays.toString(getPartialMatchTable("AA"))); //  [0, 1]
+        System.out.println(Arrays.toString(getPartialMatchTable("AB"))); // [0, 0]
+        System.out.println(Arrays.toString(getPartialMatchTable("ABCAB"))); //  [0, 0, 0, 1, 2]
         // move steps
         System.out.println("----");
-        System.out.println(moveSteps("AB", 1));
-        System.out.println(moveSteps("AAB", 2));
-        System.out.println(moveSteps("ABX", 2));
-        System.out.println(moveSteps("ABCABN", 5));
+        System.out.println(moveSteps("AB", 1)); // 1
+        System.out.println(moveSteps("AAB", 2)); // 1
+        System.out.println(moveSteps("ABX", 2)); // 2
+        System.out.println(moveSteps("ABCABN", 5)); // 3
         System.out.println("----");
         // KMP
-        System.out.println(KMP("ABCABD", "CA"));
-        System.out.println(KMP("ABAABAAC", "CA"));
-        System.out.println(KMP("ABAABAAC", "ABAAC"));
-        System.out.println(KMP("ABYABYABYABZ", "ABYABYABZ"));
-        System.out.println(KMP("AAAAB", "AAAB"));
-        System.out.println(KMP("AAAAB", ""));
-        System.out.println(KMP("", ""));
-        System.out.println(KMP("", "AA"));
-        System.out.println(KMP("ABC ABCDAB ABCDABCDABDE", "ABCDABD"));
+        System.out.println(KMP("ABCABD", "CA")); // 2
+        System.out.println(KMP("ABAABAAC", "CA")); // -1
+        System.out.println(KMP("ABAABAAC", "ABAAC")); // 3
+        System.out.println(KMP("ABYABYABYABZ", "ABYABYABZ")); // 3
+        System.out.println(KMP("AAAAB", "AAAB")); // 1
+        System.out.println(KMP("AAAAB", "")); // 0
+        System.out.println(KMP("", "")); // 0
+        System.out.println(KMP("", "AA")); // -1
+        System.out.println(KMP("ABC ABCDAB ABCDABCDABDE", "ABCDABD")); // 15
+        System.out.println(KMP("CGTGCCTACTTACTTACTTACTTACGCGAA", "CTTACTTAC"));//8
+        System.out.println(KMP("BBBBBBBBBB", "ABBBB"));//-1
     }
 }
