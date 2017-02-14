3335a144dd4573e3c5e73787cf114c556a78fea5
Bruce Zu <bruce.zu.run10@gmail.com>
2017-02-13 16:59:13 -0800

    Polish Dijkstra Shortest Path

--- arrows/src/main/java/graph/shortestpath/weightedgraphs/DijkstraShortestPath.java
+++ arrows/src/main/java/graph/shortestpath/weightedgraphs/DijkstraShortestPath.java
@@ -15,80 +15,192 @@
 
 package graph.shortestpath.weightedgraphs;
 
-import java.util.Set;
-import java.util.Stack;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 
 /**
  * <pre>
- * 1. If a given node has not path to his neighbor node, set the distance to be infinity, Integer.MAX_VALUE;
- * 2. Set start node 'td', 'tentative distance', as 0; 'from node' as null.
- *    Set other node's 'td', 'tentative distance', as infinity, Integer.MAX_VALUE; 'from node' as null.
+ * Scenario:
+ * provide: start node and end node;
+ *          It maybe a directed graph or undirected graph.
+ * confirm if it is not a spanning tree: it there is circle or the end node is a single vertex
+ *          (A spanning tree is a subset of Graph G, which has all the vertices covered with
+ *          minimum possible number of edges.
+ *          Hence, a spanning tree does not have cycles and it cannot be disconnected.)
  *
- * Idea:
- * From start node recursively call:
- *  1 Calculate all its neighbor nodes' td and from. keep the one with smallest td
- *  2 Mark current node as visited
- *  3 Check if it is the time to stop
- *     a. 'to' node is visited
- *     or
- *     b 'std is still Integer.MAX_VALUE', when there is no connection between the start node and
- *     remaining unvisited nodes.
+ * Find shortest path or check if there is path between the start node and the end node.
+ *
+ * Data structure
+ * Node:
+ *    map: immediate neighbors and the edge length/distance.
+ *    int and node: keep tentative shortest path' distance from start node to this node
+ *                  and the related predecessor node on the shortest path.
+ *
+ *    Initial:
+ *     -- map: If a given node has not path to his neighbor node, set the distance to be infinity: Integer.MAX_VALUE;
+ *     -- int and node:
+ *         for start node: 'td', 'tentative shortest distance', is 0;
+ *                         'predecessor node' is null.
+ *         for other node: 'td', 'tentative shortest distance', is Integer.MAX_VALUE;
+ *                         'predecessor node' is null.
+ *
+ * Set<Node>:  settled nodes, have found the shortest paths from start node to these nodes .
+ * Heap<Node>: sortedBorder, keep nodes under evaluating. a border between settled node and left nodes. Set<Node> borders
+ *             is for performance concern.
+ * Stack<Node> or StringBuilder:  used to calculate the shortest path.
+ *
+ * Algorithm:
+ *
+ * From start node recursively:
+ *  1 Calculate current node neighbor nodes' tentative shortest distance and predecessor node, put them in bounders
+ *  2 put current node into settled nodes
+ *  3 if 'current node' is end node. stop
+ *  4 get the top of head as the next current node
+ *      if its tentative shortest path' distance is still Integer.MAX_VALUE',
+ *      that means here is no connection between the start node and the remaining unvisited nodes.
+ *      return "there is no path to end node"
+ *      else continue loop
  *
  * @see <a href="https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm"> wiki</a>
  */
 
-
 public class DijkstraShortestPath {
-    private static void visit(Node cur, Node to, Set<Node> visted) {
-        // Smallest tentative distance from start node
-        int std = Integer.MAX_VALUE;
-        Node stdn = null;
+    private static boolean calculateShortestPath(Node start, Node end) {
+        Queue<Node> sortedBorderNodes = new PriorityQueue(); // top is with shortest distance from start
+        Set<Node> borderNodes = new HashSet<>();
+        
+        Set<Node> settled = new HashSet();
 
-        for (Node c : cur.nds.keySet()) {
-            if (!visted.contains(c)) {
-                int td = cur.nds.get(c) + cur.td;
-                if (c.td > td) {
-                    c.td = td;
-                    c.from = cur;
-                }
-                if (c.td < std) {
-                    std = c.td;
-                    stdn = c;
-                }
+        sortedBorderNodes.offer(start);
+        while (true) {
+            if (sortedBorderNodes.isEmpty()) {
+                return false;
             }
-        }
+            Node toBeSettled = sortedBorderNodes.poll();
+            borderNodes.remove(toBeSettled);
 
-        visted.add( cur);
-        if (!(cur == to || std == Integer.MAX_VALUE)) {
-            visit(stdn, to, visted);
+            // uniform-cost search
+            if (toBeSettled.tentativeShortestDistanceFromStart == Integer.MAX_VALUE) {
+                return false;   // check this before the following check
+            }
+
+            if (toBeSettled == end) {
+                return true;
+            }
+            // update neighbors tentative shortest distance from start
+            for (Node neighbor : toBeSettled.distanceToAdjacentNode.keySet()) {
+                if (!settled.contains(neighbor)) {
+                    int viaCur =
+                            toBeSettled.tentativeShortestDistanceFromStart
+                                    + toBeSettled.distanceToAdjacentNode.get(neighbor);
+                    if (viaCur < 0) { // distance may be MAX_VALUE
+                        viaCur = Integer.MAX_VALUE;
+                    }
+                    if (viaCur < neighbor.tentativeShortestDistanceFromStart) {
+                        neighbor.tentativeShortestDistanceFromStart = viaCur;
+                        neighbor.predecessorNode = toBeSettled;
+                    }
+                    if (!borderNodes.contains(neighbor)) {   //it maybe has been there. avoid repeating calculate
+                        sortedBorderNodes.offer(neighbor);
+                        borderNodes.add(neighbor);
+                    }
+                }
+
+            }
+            // settled
+            settled.add(toBeSettled);
         }
     }
 
-    public static String shortestPath(Node from, Node to, Set<Node> unvisit) {
-        from.from = null;
-
-        visit(from, to, unvisit);
-
-        // trace back to from along the result road
-        Stack<String> path = new Stack();
-        path.push(to.name);
-        Node n = to;
-        while (n.from != null) {
-            path.push(n.from.name);
-            n = n.from;
-        }
-
-        // print result
+     static String getShortestPath(Node end) {
+        // trace back to start node along the shortest path from end
         StringBuilder r = new StringBuilder();
-        r.append("Shortest path is ");
-        boolean first = true;
-        while (!path.empty()) {
-            if (first) {
-                r.append(path.pop());
-                first = false;
-            }
-            r.append(" -> ").append(path.pop());
+        r.append(end.name);
+        Node n = end;
+        while (n.predecessorNode != null) {
+            r.append(n.predecessorNode.name);
+            n = n.predecessorNode;
         }
-        return r.toString();
+        return r.reverse().toString();
+    }
+
+    // Assume all nodes has been initialed
+    // calculate shortest path from start node to end node
+    // if there is not path between them, return false.
+    public static String shortestPath(Node start, Node end) {
+        boolean hasPath = calculateShortestPath(start, end);
+        if (hasPath) {
+            return getShortestPath(end);
+        } else {
+            return "this not path betwen start " + start.name + " and end " + end.name;
+        }
+    }
+
+    // -------------------------------------------------------------
+    public static void main(String[] args) {
+        // todo test dircted graph;
+
+        /**
+         * <pre>
+         *          f    -- 9 --  e
+         *        /   \             \
+         *       /     \             \
+         *     14       2            6
+         *     /         \            \
+         *  a   - 9 -     c   - 11 -   d
+         *   \           /           /
+         *    7        10          15
+         *      \      /        /
+         *       \    /      /
+         *            b
+         */
+        Map<Node, Integer> startNodeDistanceTo = new HashMap();
+        Map<Node, Integer> bNodeDistanceTo = new HashMap();
+        Map<Node, Integer> cNodeDistanceTo = new HashMap();
+        Map<Node, Integer> dNodeDistanceTo = new HashMap();
+        Map<Node, Integer> eNodeDistanceTo = new HashMap();
+        Map<Node, Integer> fNodeDistanceTo = new HashMap();
+
+        Node start = new Node("a", startNodeDistanceTo, 0);
+        Node b = new Node("b", bNodeDistanceTo);
+        Node c = new Node("c", cNodeDistanceTo);
+        Node e = new Node("e", eNodeDistanceTo);
+        Node f = new Node("f", fNodeDistanceTo);
+        Node end = new Node("d", dNodeDistanceTo);
+
+        startNodeDistanceTo.put(b, 7);
+        startNodeDistanceTo.put(c, 9);
+        startNodeDistanceTo.put(f, 14);
+
+        bNodeDistanceTo.put(start, 7);
+        bNodeDistanceTo.put(c, 10);
+        // bNodeDistanceTo.put(c, Integer.MAX_VALUE); TEST 1
+        bNodeDistanceTo.put(end, 15);
+        // bNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2   this not path between start a and end d
+
+        cNodeDistanceTo.put(start, 9);
+        cNodeDistanceTo.put(b, 10);
+        cNodeDistanceTo.put(end, 11);
+        // cNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2
+        cNodeDistanceTo.put(f, 2);
+
+        fNodeDistanceTo.put(start, 14);
+        fNodeDistanceTo.put(c, 2);
+        fNodeDistanceTo.put(e, 9);
+
+        eNodeDistanceTo.put(f, 9);
+        eNodeDistanceTo.put(end, 6);
+        // eNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2
+
+        dNodeDistanceTo.put(e, 6);
+        // dNodeDistanceTo.put(e, Integer.MAX_VALUE); // TEST 2
+        dNodeDistanceTo.put(c, 11);
+        // dNodeDistanceTo.put(c, Integer.MAX_VALUE); // TEST 2
+        dNodeDistanceTo.put(b, 15);
+        // dNodeDistanceTo.put(b, Integer.MAX_VALUE); // TEST 2
+
+        System.out.println(DijkstraShortestPath.shortestPath(start, end));
     }
 }
---
+++ arrows/src/main/java/graph/shortestpath/weightedgraphs/DijkstraShortestPath2.java
@@ -0,0 +1,158 @@
+//  Copyright 2017 The keepTry Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package graph.shortestpath.weightedgraphs;
+
+import java.util.*;
+
+import static graph.shortestpath.weightedgraphs.DijkstraShortestPath.getShortestPath;
+
+/**
+ * <pre>
+ * Difference with DijkstraShortestPath is the parameter of methods
+ * provided the Set<Node>graph.
+ *
+ * pros:
+ * with it the code becomes concise with one Heap is enough
+ * Set<Node> settled is saved too
+ *
+ * cons:
+ * 1 initially all nodes are entered into the priority queue. This is, however, not necessary
+ * see DijkstraShortestPath, especially when memory is not enough.
+ * 2 resort each loop. Heap (Priority can not resort automatically when some element changed value)
+ *
+ * O(|E|+|V|log|V|) for connected graphsin the worst case;.
+ * the number of edges, denoted  |E|,
+ * and the number of vertices, denoted |V|
+ * When using binary heaps, the average case time complexity is lower than the worst-case.
+ */
+public class DijkstraShortestPath2 {
+
+    public static boolean calculateShortestPath(Set<Node> graph, Node end) {
+        Queue<Node> unSettled = new PriorityQueue();
+
+        for (Node n : graph) {
+            unSettled.offer(n);
+        }
+        while (!unSettled.isEmpty()) {
+            Node toBeSttled = unSettled.poll();
+            if (toBeSttled.tentativeShortestDistanceFromStart == Integer.MAX_VALUE) {
+                return false;
+            }
+            if (toBeSttled == end) {
+                return true;
+            }
+            for (Node neighbor : toBeSttled.distanceToAdjacentNode.keySet()) {
+                int alt = toBeSttled.tentativeShortestDistanceFromStart
+                        + toBeSttled.distanceToAdjacentNode.get(neighbor);
+                if (alt < 0) {
+                    alt = Integer.MAX_VALUE;
+                }
+                if (alt < neighbor.tentativeShortestDistanceFromStart) {
+                    neighbor.tentativeShortestDistanceFromStart = alt;
+                    neighbor.predecessorNode = toBeSttled;
+
+                   if(unSettled.contains(neighbor)){
+                       unSettled.remove(neighbor);
+                       unSettled.offer(neighbor);
+                   }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Assume all nodes has been initialed
+    // calculate shortest path from start node to end node
+    // if there is not path between them, return false.
+    public static String shortestPath(Set<Node> graph, Node start, Node end) {
+        boolean hasPath = calculateShortestPath(graph, end);
+        if (hasPath) {
+            return getShortestPath(end);
+        } else {
+            return "this not path betwen start " + start.name + " and end " + end.name;
+        }
+    }
+
+    // -------------------------------------------------------------
+    public static void main(String[] args) {
+        // todo test dircted graph;
+        // todo test: it will be wrong if does not resort the border nodes
+
+        /**
+         * <pre>
+         *          f    -- 9 --  e
+         *        /   \             \
+         *       /     \             \
+         *     14       2            6
+         *     /         \            \
+         *  a   - 9 -     b   - 11 -   d
+         *   \           /           /
+         *    7        10          15
+         *      \      /        /
+         *       \    /      /
+         *            c
+         */
+        Map<Node, Integer> startNodeDistanceTo = new HashMap();
+        Map<Node, Integer> bNodeDistanceTo = new HashMap();
+        Map<Node, Integer> cNodeDistanceTo = new HashMap();
+        Map<Node, Integer> dNodeDistanceTo = new HashMap();
+        Map<Node, Integer> eNodeDistanceTo = new HashMap();
+        Map<Node, Integer> fNodeDistanceTo = new HashMap();
+
+        Node start = new Node("a", startNodeDistanceTo, 0);
+        Node b = new Node("b", cNodeDistanceTo);
+        Node e = new Node("e", eNodeDistanceTo);
+        Node c = new Node("c", bNodeDistanceTo);
+        Node f = new Node("f", fNodeDistanceTo);
+        Node end = new Node("d", dNodeDistanceTo);
+
+        startNodeDistanceTo.put(b, 7);
+        startNodeDistanceTo.put(c, 9);
+        startNodeDistanceTo.put(f, 14);
+
+        bNodeDistanceTo.put(start, 7);
+        // bNodeDistanceTo.put(c, 10);
+        bNodeDistanceTo.put(c, Integer.MAX_VALUE); // TEST 1
+        bNodeDistanceTo.put(end, 15);
+        // bNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2   this not path between start a and end d
+
+        cNodeDistanceTo.put(start, 9);
+        cNodeDistanceTo.put(b, 10);
+        cNodeDistanceTo.put(end, 11);
+        // cNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2
+        cNodeDistanceTo.put(f, 2);
+
+        fNodeDistanceTo.put(start, 14);
+        fNodeDistanceTo.put(c, 2);
+        fNodeDistanceTo.put(e, 9);
+
+        eNodeDistanceTo.put(f, 9);
+        eNodeDistanceTo.put(end, 6);
+        // eNodeDistanceTo.put(end, Integer.MAX_VALUE); // TEST 2
+
+        dNodeDistanceTo.put(e, 6);
+        // dNodeDistanceTo.put(e, Integer.MAX_VALUE); // TEST 2
+        dNodeDistanceTo.put(c, 11);
+        // dNodeDistanceTo.put(c, Integer.MAX_VALUE); // TEST 2
+        dNodeDistanceTo.put(b, 15);
+        // dNodeDistanceTo.put(b, Integer.MAX_VALUE); // TEST 2
+
+        System.out.println(shortestPath(
+                new HashSet<>(Arrays.asList(start, e, end, b, c, f)),
+                start,
+                end));
+    }
+}
