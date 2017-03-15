2c4a3ae59a2615ea59e48bfbcbb6153df505e435
Bruce Zu <bruce.zu.run10@gmail.com>
2017-03-15 09:09:50 -0700

    Leetcode 155 Min Stack

--- arrows/src/main/java/leetcode/Leetcode155MinStack.java
+++ arrows/src/main/java/leetcode/Leetcode155MinStack.java
@@ -15,6 +15,8 @@
 
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * <pre>
  * stack. its pop, push, getMin ‘algorithm complexity is O1
@@ -23,9 +25,75 @@ package leetcode;
  *
  * @see <a href="https://leetcode.com/problems/min-stack/">leetcode</a>
  */
-
 public class Leetcode155MinStack {
 
-}
+    public class MinStack {
+        long min;
+        Stack<Long> stack; //   Integer.MAX_VALUE-Integer.MIN_VALUE;
 
+        public MinStack() {
+            stack = new Stack<>();
+        }
+
+        public void push(int cur) {
+            if (stack.isEmpty()) {
+                stack.push(0L);
+                min = cur;
+            } else {
+                stack.push(cur - min);//Could be negative if min value needs to change
+                if (cur < min) {
+                    min = cur;
+                }
+            }
+        }
+
+        public void pop() {
+            if (stack.isEmpty()) {
+                return;
+            }
+            long sub = stack.pop();
+
+            if (sub < 0) {
+                min = min - sub;
+            }
+        }
+
+        public int top() {
+            long sub = stack.peek();
+            if (sub > 0) { // pre min is just current min
+                return (int) (sub + min);
+            } else {
+                //   < 0 : cur less than the pre min and cur is just the cur min;
+                //   = 0 : cur is pre min and is cur min.
+                return (int) (min);
+            }
+        }
+        public int getMin() {
+            return (int)min;
+        }
+    }
+
+    // ---------------- space need more only the code is concise
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack< >();
+    public void push(int cur) {
+        if(cur <= min){
+            stack.push(min);
+            min=cur;
+        }
+        stack.push(cur);
+    }
+
+    public void pop() {
+        if(stack.pop() == min) {min=stack.pop();}
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
 
