c8032a79740f2dd31c7ccb6fe74022be9f733b9f
Bruce Zu <bruce.zu.run10@gmail.com>
2017-03-17 22:33:10 -0700

    Leetcode 406 Queue Reconstruction by Height

---
+++ arrows/src/main/java/greedy/Leetcode406QueueReconstructionbyHeight.java
@@ -0,0 +1,32 @@
+//  Copyright 2017 The keepTry Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode406QueueReconstructionbyHeight {
+    // o(n^2)
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            list.add(people[i][1], people[i]);
+        }
+        return list.toArray(people);
+    }
+}
