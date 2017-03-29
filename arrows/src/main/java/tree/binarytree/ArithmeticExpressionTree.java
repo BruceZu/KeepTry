05d022b0a875b9f3b1872c84cf3f39aaa239ba61
Bruce Zu <bruce.zu.run10@gmail.com>
2017-03-28 17:44:30 -0700

    Arithmetic expression tree/evaluator

---
+++ arrows/src/main/java/tree/binarytree/ArithmeticExpressionTree.java
@@ -0,0 +1,52 @@
+//  Copyright 2017 The keepTry Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package tree.binarytree;
+
+/**
+ * <pre>
+ * a + b - e * f * g
+ * ->
+ * "a b + e f * g * -"
+ *
+ * 1 + 2 - 3 * 4 / 5
+ * ( 1 * ( 2 + 2 ) - ( 3 - 4 ) / 5 )
+ *
+ *
+ * https://en.wikipedia.org/wiki/Binary_expression_tree
+ * http://penguin.ewu.edu/class/cscd300/Topic/ExpressionTree/index.html
+ * http://stackoverflow.com/questions/4589951/parsing-an-arithmetic-expression-and-building-a-tree-from-it-in-java
+ * https://theantlrguy.atlassian.net/wiki/display/ANTLR3/Five+minute+introduction+to+ANTLR+3
+ * http://www.geeksforgeeks.org/expression-tree/
+ */
+public class ArithmeticExpressionTree {
+    // todo
+}
+
+class ArithmeticExpressionEvaluator {
+    /**
+     * <pre>
+     * http://codinghelmet.com/?path=exercises/expression-evaluator
+     *
+     * 1> table of boolean[][].
+     * 2> using 2 stacks
+     * 3> single-digit number and multi-digit number in arithmetic expression
+     * 4> wrap with open/close parenthesis
+     * 5> close parenthesis action.
+     */
+    int evaluate(String[] arithmeticExpression) {
+        return 0; // todo
+    }
+}
