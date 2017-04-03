136c8262a981efd4a4191f456a147221bace32ae
Bruce Zu <bruce.zu.run10@gmail.com>
2017-04-03 13:53:38 -0700

    Annotation

---
+++ bow/2-technical-and-design/jdk8/src/main/java/annotation/package-info.java
@@ -0,0 +1,26 @@
+//  Copyright 2017 The keepTry Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/**
+ * Policy:
+ * 1 source
+ *     lombok→ get, set,toString, Constructor
+ *     javax.annotatation.processing.*   processor
+ * 2 class
+ *   ASM
+ *   javap -v <classname
+ * 3 runtime
+ *   
+ */
+package annotation;
