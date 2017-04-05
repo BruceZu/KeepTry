9377ceef76e93e4e66d7f65608fb31b01b8e3035
Bruce Zu <bruce.zu.run10@gmail.com>
2017-04-04 20:52:38 -0700

    Generics

---
+++ bow/2-technical-and-design/jdk8/src/main/java/generics/Generics.java
@@ -0,0 +1,57 @@
+//  Copyright 2017 The keepTry Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package generics;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Generics {
+
+    public static void main(String[] args) {
+        // 1
+        Class a = new ArrayList<Integer>().getClass();
+        Class b = new ArrayList<String>().getClass();
+        System.out.println(a == b); // true
+
+
+        // 2
+        //    Collections
+        //    public static <T> void copy(List<? super T> dest, List<? extends T> src) {
+
+        // 3
+        List<Integer>[] listArray = new ArrayList[1];
+        ArrayList<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+
+        listArray[0] = list;
+        for (List<Integer> e : listArray) {
+            for (Integer i : e) {
+                System.out.println(i);
+            }
+        }
+
+        // 4
+        //  if(listArray instanceof   ArrayList<Integer>[] ){ //
+        if (listArray instanceof ArrayList[]) {
+        }
+        if (listArray instanceof ArrayList<?>[]) {
+        }
+    }
+}
