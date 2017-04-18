8c78fa5accdb11c7baf1847c014ddd78621489aa
Bruce Zu <bruce.zu.run10@gmail.com>
2017-04-18 08:20:53 -0700

    Distributed Algorithms

---
+++ ParallelAlgorithms/src/package-info.java
@@ -0,0 +1,8 @@
+//
+// Parallel Processing and Parallel Algorithms: Theory and Computation
+//        Seyed H Roosta - Computers - 2012 - 566 pages
+// Parallel computing  Author:	M R Bhujade
+//
+// https://en.wikipedia.org/wiki/Parallel_computing
+// https://en.wikipedia.org/wiki/Computational_complexity_theory
+// https://www.reddit.com/r/compsci/comments/rqim5/how_does_parallel_programming_affect_an/
