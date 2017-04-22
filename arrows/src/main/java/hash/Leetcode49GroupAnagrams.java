ad6eee4608a5f1050337749d9ab4e63d4527362e
Bruce Zu <bruce.zu.run10@gmail.com>
2017-04-22 11:30:15 -0700

    Leetcode 49 Group Anagrams

--- arrows/src/main/java/hash/Leetcode49GroupAnagrams.java
+++ arrows/src/main/java/hash/Leetcode49GroupAnagrams.java
@@ -15,6 +15,8 @@
 
 package hash;
 
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -28,7 +30,7 @@ import java.util.Map;
  */
 public class Leetcode49GroupAnagrams {
     // too slow
-    public static List<List<String>> groupAnagrams2(String[] strs) {
+    public static List<List<String>> groupAnagrams3(String[] strs) {
         Map<Map<Character, Integer>, List<String>> r = new HashMap();
         for (int i = 0; i < strs.length; i++) {
             String str = strs[i];
@@ -48,22 +50,47 @@ public class Leetcode49GroupAnagrams {
         return new ArrayList<>(r.values());
     }
 
-    // 19 ms
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> r = new HashMap();
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
-            char[] id = strs[i].toCharArray();
-            Arrays.sort(id);
-            String sid = new String(id);
-            if (!r.containsKey(sid)) {
-                r.get(sid).add(str);
-            } else {
-                List<String> s = new ArrayList();
-                s.add(str);
-                r.put(sid, new ArrayList( ));
+    // 19 ms O(?)
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String hash = new String(chars);
+
+            List<String> l = map.get(hash);
+            if (l == null) {
+                l = new ArrayList();
+                map.put(hash, l);
             }
+            l.add(str);
         }
-        return new ArrayList<>(r.values());
+        return new ArrayList(map.values());
+    }
+
+    static public List<List<String>> groupAnagrams(String[] strs) {
+        int[] prime = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+                37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
+                79, 83, 89, 97, 101, 103}; // 26 letter. start from 3. Not 2.
+
+        Map<Integer, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int hash = 1;
+            for (char c : str.toCharArray()) {
+                hash *= prime[c - 'a'];
+            }
+
+            List<String> l = map.get(hash);
+            if (l == null) {
+                l = new ArrayList();
+                map.put(hash, l);
+            }
+            l.add(str);
+        }
+        return new ArrayList(map.values());
+    }
+
+    public static void main(String[] args) {
+        groupAnagrams(new String[]{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"});
     }
 }
