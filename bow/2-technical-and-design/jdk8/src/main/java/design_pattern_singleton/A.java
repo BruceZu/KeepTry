86c261c56b45f09ab401ba1760118a890751d94a
Bruce Zu <bzu@fortinet.com>
2017-10-18 20:35:20 -0700

    Doc  Enum based singleton

---
+++ bow/2-technical-and-design/jdk8/src/main/java/design_pattern_singleton/A.java
@@ -0,0 +1,28 @@
+package design_pattern_singleton;
+
+class B {
+    // http://www.artima.com/insidejvm/ed2/lifetype.html
+    // enum instance fields are not "initialized  by a compile-time constant expression".
+    // They can't be.
+
+    // because only String and primitive types are possible types for a compile-time constant expression.
+    // That means that the class will be initialized when INSTANCE is first accessed
+    // (which is  exactly the desired effect).
+
+    // if the field is 'static final', the initialzation of the field is happened
+    // at complie-time
+
+    public static final String FOO = "foo";
+    public static final Object obj = new Object(); //initialised at runtime
+
+    static {
+        System.out.println("initializing B");
+    }
+}
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println(B.FOO);
+        System.out.println(B.obj);
+    }
+}
