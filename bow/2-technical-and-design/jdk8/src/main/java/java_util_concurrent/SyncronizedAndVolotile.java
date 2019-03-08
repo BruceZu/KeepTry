0a3fa4302f93249351294858d2a065a4560dddc3
Bruce Zu <bzu@fortinet.com>
2019-03-07 23:40:16 -0800

    Compare Atomicxx with volatile and synchronized
    Atomicxx is visible and atomic.

--- bow/2-technical-and-design/jdk8/src/main/java/java_util_concurrent/SyncronizedAndVolotile.java
+++ bow/2-technical-and-design/jdk8/src/main/java/java_util_concurrent/SyncronizedAndVolotile.java
@@ -16,8 +16,10 @@
 package java_util_concurrent;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class SyncronizedAndVolotile {
+    private static AtomicLong atomicLong = new AtomicLong(0);
     private static boolean common;
     private static volatile boolean valotiled;
     private static boolean synced;
@@ -33,6 +35,10 @@ public class SyncronizedAndVolotile {
     public static void main(String[] args) throws InterruptedException {
         Runnable[] rarray =
                 new Runnable[] {
+                    () -> {
+                        while (atomicLong.get() != 0) {}
+                        System.out.println("end atomicLong");
+                    },
                     () -> {
                         while (!common) {}
                         System.out.println("end common");
@@ -57,9 +63,12 @@ public class SyncronizedAndVolotile {
         common = !common;
         valotiled = !valotiled;
         changeSynced();
+        atomicLong.getAndIncrement();
 
-        System.out.println("common: " + common);
+        System.out.println("\ncommon: " + common);
         System.out.println("valotiled: " + valotiled);
         System.out.println("synced: " + getSyncincrease());
+        System.out.println("atomic: " + atomicLong.get());
+        System.out.println("\nBut the thread with common is still false and not end yet");
     }
 }
