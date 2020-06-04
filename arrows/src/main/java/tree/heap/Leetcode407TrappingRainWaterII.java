c255a3fdf7d542add761028a00e9e72963706eed
Bruce Zu <zu.bruce.china@gmail.com>
2020-06-03 23:19:49 -0700

    polish BFS: Leetcode 407 Trapping Rain Water II

--- arrows/src/main/java/tree/heap/Leetcode407TrappingRainWaterII.java
+++ arrows/src/main/java/tree/heap/Leetcode407TrappingRainWaterII.java
@@ -19,76 +19,66 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-/**
- * <a href="https://leetcode.com/problems/trapping-rain-water-ii/?tab=Description">LeetCode</a>
- */
+/** <a href="https://leetcode.com/problems/trapping-rain-water-ii/?tab=Description">LeetCode</a> */
 public class Leetcode407TrappingRainWaterII {
+  static class E { // 'class' lower case
+    int r, c, h;
 
-    class Cell {
-        int r;
-        int column;
-        int height;
-
-        public Cell(int row, int col, int height) {
-            this.r = row;
-            this.column = col;
-            this.height = height;
-        }
+    public E(int r, int c, int h) {
+      this.r = r;
+      this.c = c;
+      this.h = h;
     }
+  }
+  // Get out the current water level cell, BFS to its unvisited neighbors:
+  // record visited.
+  // if the neighbor is lower,:
+  //      sure it will keep water,
+  //      assume it is filled water, update its level to current water level.
+  // add the neighbors to the bounder circle.
+  //
+  // improvement: when visited neighbor is lower the current water level cell
+  // continue BFS from there  to save the process of put it in circle and fetch it out again.
+  public int trapRainWater(int[][] heightMap) {
+    // as there is at least 2 element
+    if (heightMap == null || heightMap.length < 3 || heightMap[0].length < 3) return 0;
+    // at least is is 3x3 matrix
+    int m = heightMap.length;
+    int n = heightMap[0].length;
 
-    public int trapRainWater(int[][] heightMap) {
-        if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) {
-            return 0;
-        }
+    boolean[][] v = new boolean[m][n]; // default false
 
-        int rows = heightMap.length;
-        int columns = heightMap[0].length;
-        boolean[][] visited = new boolean[rows][columns];
-        Queue<Cell> circle = new PriorityQueue(1, new Comparator<Cell>() {
-            public int compare(Cell a, Cell b) {
-                return a.height - b.height;
-            }
-        }); // water level
-        // initial the border circle
-        for (int r = 0; r < rows; r++) {
-            visited[r][0] = true;
-            visited[r][columns - 1] = true;
-            circle.offer(new Cell(r, 0, heightMap[r][0]));
-            circle.offer(new Cell(r, columns - 1, heightMap[r][columns - 1]));
-        }
+    Queue<E> q = new PriorityQueue<E>((Comparator<E>) (a, b) -> a.h - b.h);
 
-        for (int col = 0; col < columns; col++) {
-            visited[0][col] = true;
-            visited[rows - 1][col] = true;
-            circle.offer(new Cell(0, col, heightMap[0][col]));
-            circle.offer(new Cell(rows - 1, col, heightMap[rows - 1][col]));
-        }
+    int[][] nb4 = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        // Get out the current water level cell, BFS to its unvisited neighbors:
-        // record visited.
-        // if the neighbor is lower,:
-        //      sure it will keep water,
-        //      assume it is filled water, update its level to current water level.
-        // add the neighbors to the bounder circle.
-        int[][] toNeighber = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int res = 0;
-        while (!circle.isEmpty()) {
-            Cell waterLevelCell = circle.poll();
-            // -- BFS
-            for (int[] toNeighbor : toNeighber) {
-                int row = waterLevelCell.r + toNeighbor[0];
-                int col = waterLevelCell.column + toNeighbor[1];
-                if (row >= 0 && row < rows && col >= 0 && col < columns && !visited[row][col]) {
-                    visited[row][col] = true;
-                    if (waterLevelCell.height > heightMap[row][col]) {// sure there is water
-                        res += waterLevelCell.height - heightMap[row][col];
-                    }
-                    circle.offer(new Cell(row, col, Math.max(heightMap[row][col], waterLevelCell.height)));
-                }
-            }
-        }
-        return res;
+    // initial q
+    for (int i = 0; i < m; i++) {
+      q.offer(new E(i, 0, heightMap[i][0]));
+      v[i][0] = true;
+      q.offer(new E(i, n - 1, heightMap[i][n - 1]));
+      v[i][n - 1] = true;
     }
-    // improvement: when visited neighbor is lower the current water level cell
-    // continue BFS from there  to save the process of put it in circle and fetch it out again.
+    for (int j = 0; j < n; j++) { // 4 corners are ignorable.
+      q.offer(new E(0, j, heightMap[0][j]));
+      v[0][j] = true;
+      q.offer(new E(m - 1, j, heightMap[m - 1][j]));
+      v[m - 1][j] = true;
+    }
+    // calculate result
+    int res = 0;
+    while (!q.isEmpty()) {
+      E e = q.poll(); // no pop method
+      for (int[] nb : nb4) {
+        int r = e.r + nb[0], c = e.c + nb[1];
+        if (0 <= r && r < m && 0 <= c && c < n && v[r][c] == false) { // valid E
+          v[r][c] = true;
+          //  there is waters only when cur elevation is lower than the polled one.
+          res += Math.max(e.h - heightMap[r][c], 0);
+          q.offer(new E(r, c, Math.max(e.h, heightMap[r][c]))); // keep the water level
+        }
+      }
+    }
+    return res;
+  }
 }
