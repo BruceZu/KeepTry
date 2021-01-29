5f7ede602b6186fd2f50bc510909f52a36e3cfad
Bruce Zu <zu.bruce.china@gmail.com>
2021-01-29 13:14:44 -0800

    Manacher algorithm

--- arrows/src/main/java/string/palindrome/Leetcode5LongestPalindromicSubstring.java
+++ arrows/src/main/java/string/palindrome/Leetcode5LongestPalindromicSubstring.java
@@ -15,88 +15,22 @@
 
 package string.palindrome;
 
+import java.util.Arrays;
+
 public class Leetcode5LongestPalindromicSubstring {
-  public String longestPalindrome(String s) {
-    if (s == null || s.length() == 0) return s;
-    int N = 2 * s.length() + 1;
-    char[] T = new char[N];
-    for (int i = 0; i < N; i++) {
-      // assume '#' is not in s
-      T[i] = (i & 1) == 1 ? s.charAt(i / 2) : '#';
-    }
-
-    int[] radius = new int[N];
-    // when index==0, for "#"
-    int maxRight = 0,
-        c = 0; // axis or center of palindrome which has the rightmost boundary character
-    int rc = 0; // first longest palindromic sub-string center index in T
-    for (int i = 1; i < N; i++) {
-      if (i <= maxRight) {
-        int mirror = 2 * c - i; // axis-(i-axis)
-        radius[i] = Math.min(maxRight - i, radius[mirror]);
-        if (radius[mirror] != maxRight - i) {
-          // need not update maxRight, axis, and result
-          continue;
-        }
-      }
-
-      int l = i - (1 + radius[i]), r = i + (1 + radius[i]);
-      while (l >= 0 && r < N && T[l] == T[r]) {
-        radius[i]++;
-        l--;
-        r++;
-      }
-
-      if (i + radius[i] > maxRight) {
-        maxRight = i + radius[i];
-        c = i;
-      }
-      // application: longest palindromic sub-string
-      if (radius[i] > radius[rc]) {
-        rc = i;
-      }
-    }
-    int f = (rc - radius[rc]) / 2;
-    return s.substring(f, f + radius[rc]);
-  }
-
+  // Manacher's algorithm with virtual translated string
   /** when the division character is not easy to find,use virtual augmented string */
   public String longestPalindrome2(String s) {
     if (s == null || s.length() == 0) return s;
     int N = 2 * s.length() + 1;
-    int[] rad = new int[N];
-    // when index==0, for virtual division character
-    int c = 0; // axis or center of palindrome which has the rightmost boundary character
-    int rc = 0; // first longest palindromic sub-string center index in T
-    for (int i = 1; i < N; i++) {
-      int rMost = c + rad[c];
-      if (i <= rMost) {
-        int mirror = 2 * c - i; // axis-(i-axis)
-        rad[i] = Math.min(rMost - i, rad[mirror]); // reuse
-        if (i + rad[mirror] != rMost) {
-          // need not update maxRight, axis, and result
-          continue;
-        }
-      }
+    int[] rad = Manacher.getRadiusOfVirtualTranslatedStringOf(s);
 
-      for (int l = i - (1 + rad[i]), r = i + (1 + rad[i]); ; ) {
-        if (l < 0 || r == N || (l & 1) == 1 && s.charAt(l / 2) != s.charAt(r / 2)) {
-          break;
-        }
-        rad[i]++;
-        l--;
-        r++;
-      }
-
-      if (i + rad[i] > c + rad[c]) {
-        c = i;
-      }
-      // application: longest palindromic sub-string
-      if (rad[i] > rad[rc]) {
-        rc = i;
-      }
+    int il /* index Of the longest radius */ = 0;
+    for (int i = 0; i < N; i++) {
+      if (rad[i] > rad[il]) il = i;
     }
-    int f = (rc - rad[rc]) / 2;
-    return s.substring(f, f + rad[rc]);
+
+    int si = (il - rad[il]) / 2;
+    return s.substring(si, si + rad[il]);
   }
 }
--- arrows/src/main/java/string/palindrome/Leetcode647PalindromicSubstrings.java
+++ arrows/src/main/java/string/palindrome/Leetcode647PalindromicSubstrings.java
@@ -18,36 +18,9 @@ package string.palindrome;
 public class Leetcode647PalindromicSubstrings {
   public static int countSubstrings(String s) {
     if (s == null || s.length() == 0) return 0;
-    int N = 2 * s.length() + 1;
-
-    // Manacher's algorithm to calculate the radius of palindrome centered on i in virtual string T.
-    int r[] = new int[N];
-    int c = 0;
-    for (int i = 1; i < N; i++) {
-      int rMost = c + r[c];
-      if (i <= rMost) {
-        int mirri = c - (i - c); // mirror of i
-        r[i] = Math.min(r[mirri], rMost - i); // reuse in all 3 scenario
-        if (i + r[mirri] != rMost) {
-          // do not calculate the sum here and only calculate radius, just only do Mancher's
-          // algorithm need to do.
-          continue;
-        }
-      }
-      // explore
-      int l = i - r[i] - 1, e = i + r[i] + 1;
-      // virtual T and S: index relation. even index in T is division character, si=ti/2
-      while (0 <= l && e < N && ((l & 1) == 0 || s.charAt(l / 2) == s.charAt(e / 2))) {
-        l--;
-        e++;
-        r[i]++;
-      }
-      if (i + r[i] > c + r[c]) {
-        c = i;
-      }
-    }
-    // Manacher's algorithm end
+    int r[] = Manacher.getRadiusOfVirtualTranslatedStringOf(s);
     int sum = 0;
+    int N = 2 * s.length() + 1;
     for (int i = 0; i < N; i++) {
       if (r[i] != 0) {
         int w = r[i];
