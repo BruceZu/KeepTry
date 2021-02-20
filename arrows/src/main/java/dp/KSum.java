86feefbfda444054fef2088ab02c5d3a9bfc364f
Bruce Zu <zu.bruce.china@gmail.com>
2021-02-20 15:12:46 -0800

    polish Ksum

--- arrows/src/main/java/dp/KSum.java
+++ arrows/src/main/java/dp/KSum.java
@@ -19,77 +19,71 @@ package dp;
  * from distinct and positive integer array find k number to make their sum is target, k < = length
  * ask the number of solutions.
  */
+// bottom up
 public class KSum {
-    // Get count of solution
-    static int ksum(int[] arr, int k, int target) {
-        int[][][] dp = new int[arr.length + 1][k + 1][target + 1];
+  static int ksum(int[] arr, int K, int target) {
+    int N = arr.length;
+    int[][][] dp = new int[N + 1][K + 1][target + 1];
 
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int t = 0; t <= target; t++) {
-                    if (j == 0 && t == 0) {
-                        // i j k
-                        // x 0 0
-                        // 0 0 0
-                        dp[i][j][t] = 1;
-                    } else if (i != 0 && j != 0 && t != 0) {
-                        // i j t
-                        // x x x
-                        dp[i][j][t] = dp[i - 1][j][t];
-                        if (t - arr[i - 1] >= 0) {
-                            dp[i][j][t] += dp[i - 1][j - 1][t - arr[i - 1]];
-                        }
-                    }
-                    // i, j, t
-                    // 0  x  x
-                    // x  0  x
-                    // x  x  0
-                    // 0  0  x
-                    // 0  x  0
-
-                }
+    for (int i = 0; i <= N; i++) { // index in dp, not in arr.
+      for (int k = 0; k <= K; k++) {
+        for (int t = 0; t <= target; t++) {
+          if (k == 0 && t == 0) { // no matter i is 0 or not.
+            dp[i][k][t] = 1;
+          } else if (i != 0 && k != 0 && t != 0) {
+            dp[i][k][t] = dp[i - 1][k][t];
+            if (t - arr[i - 1] >= 0) {
+              dp[i][k][t] += dp[i - 1][k - 1][t - arr[i - 1]];
             }
+          }
         }
-
-        return dp[arr.length][k][target];
+      }
     }
 
-    static int ksum2(int[] arr, int k, int target) {
-        int[][][] dp = new int[arr.length + 1][k + 1][target + 1];
-        for (int i = 0; i <= arr.length; i++) dp[i][0][0] = 1;
+    return dp[N][K][target];
+  }
 
-        for (int i = 1; i <= arr.length; i++) {
-            for (int j = 1; j <= k; j++) {
-                for (int t = 1; t <= target; t++) {
-                    dp[i][j][t] = dp[i - 1][j][t];
-                    if (t - arr[i - 1] >= 0) {
-                        dp[i][j][t] += dp[i - 1][j - 1][t - arr[i - 1]];
-                    }
-                }
-            }
+  // bottom up O(NKT)
+  static int ksum2(int[] arr, int K, int target) {
+    int N = arr.length;
+    int[][][] dp = new int[N + 1][K + 1][target + 1];
+    // default dp[i][k][t] is 0
+    // dp[x][0][0] is 1. the meaning comes from  top down idea. See ksum3.
+    for (int i = 0; i <= N; i++) dp[i][0][0] = 1;
+
+    for (int i = 1; i <= N; i++) {
+      for (int k = 1; k <= K; k++) {
+        for (int t = 1; t <= target; t++) {
+          dp[i][k][t] = dp[i - 1][k][t];
+          if (t - arr[i - 1] >= 0) {
+            dp[i][k][t] += dp[i - 1][k - 1][t - arr[i - 1]];
+          }
         }
-
-        return dp[arr.length][k][target];
+      }
     }
 
-    static int ksum3(int[] arr, int i, int j, int k) {
-        if (j > i) return 0;
-        // now j <= i
-        if (j == 0) {
-            if (k == 0) return 1;
-            return 0;
-        }
-        // j!=0
-        return ksum3(arr, i - 1, j, k) + ksum3(arr, i - 1, j - 1, k - arr[i - 1]);
-    }
+    return dp[N][K][target];
+  }
 
-    public static void main(String[] args) {
-        System.out.println(ksum(new int[] {1, 2, 3, 4}, 2, 5) == 2);
-        System.out.println(ksum2(new int[] {1, 2, 3, 4}, 2, 5) == 2);
-        System.out.println(ksum3(new int[] {1, 2, 3, 4}, 4, 2, 5) == 2);
-
-        System.out.println(ksum(new int[] {1, 2, 3, 4, 5}, 3, 6) == 1);
-        System.out.println(ksum2(new int[] {1, 2, 3, 4, 5}, 3, 6) == 1);
-        System.out.println(ksum3(new int[] {1, 2, 3, 4, 5}, 5, 3, 6) == 1);
+  // top - down idea
+  static int ksum3(int[] arr, int l, int k, int target) {
+    if (l < k) return 0;
+    // now k <= l
+    if (k == 0) {
+      if (target == 0) return 1;
+      return 0;
     }
+    // k != 0, continue recursion convert
+    return ksum3(arr, l - 1, k, target) + ksum3(arr, l - 1, k - 1, target - arr[l - 1]);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(ksum(new int[] {1, 2, 3, 4}, 2, 5) == 2);
+    System.out.println(ksum2(new int[] {1, 2, 3, 4}, 2, 5) == 2);
+    System.out.println(ksum3(new int[] {1, 2, 3, 4}, 4, 2, 5) == 2);
+
+    System.out.println(ksum(new int[] {1, 2, 3, 4, 5}, 3, 6) == 1);
+    System.out.println(ksum2(new int[] {1, 2, 3, 4, 5}, 3, 6) == 1);
+    System.out.println(ksum3(new int[] {1, 2, 3, 4, 5}, 5, 3, 6) == 1);
+  }
 }
