27f6f54628c2a3f59c8f6bccfe8e3b3b42ef16ec
Bruce Zu <zu.bruce.china@gmail.com>
2021-06-22 13:59:31 -0700

    leetcode 713

--- arrows/src/main/java/two_pointer/Leetcode713SubarrayProductLessThanK.java
+++ arrows/src/main/java/two_pointer/Leetcode713SubarrayProductLessThanK.java
@@ -17,36 +17,85 @@ package two_pointer;
 
 import java.util.Arrays;
 
-public class Leetcode {
+public class Leetcode713SubarrayProductLessThanK {
+  /*
+  Ask:
+     an array of positive integers nums and an integer k,
+     return the number of contiguous subarrays where
+     the product of all the elements in the subarray is strictly less than k.
+     Note:
 
-  public int numSubarrayProductLessThanK(int[] nums, int K) {
-    if (K == 0) return 0;
-    double k = K;
-    double[] pre = new double[nums.length];
-    pre[0] = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-      pre[i] = pre[i - 1] * nums[i];
-    }
+      1 <= nums.length <= 3 * 10^4
+      1 <= nums[i] <= 1000
+      0 <= k <= 10^6
 
-    int a = 0;
-    for (int i = 0; i < pre.length; i++) {
-      int l = i + 1, r = pre.length - 1;
-      int tmp = Arrays.binarySearch(pre, l, r + 1, i == 0 ? k : k / pre[i - 1]);
-      if (tmp < 0) {
-        tmp = ~tmp;
-        if (tmp == i + 1) a += 1;
-        else a += tmp - i;
-      } else {
-        a += tmp - i;
-      }
+    Idea:
+      k can be 0, 1
+      nums[i] is positive integer. not 0 or negative
+   */
+  /*
+  -----------------------------------------------------------------------------
+  Slide window:
+    based on no-decreasing product of sub array based on positive int array
+  A:
+    calculate the number of subarray with same right element
+    O(N) time, O(1) space
+   */
+  public int numSubarrayProductLessThanK(int[] nums, int k) {
+    int p = 1, l = 0, a = 0;
+    for (int r = 0; r < nums.length; r++) {
+      p *= nums[r];
+      while (p >= k && l < nums.length) p /= nums[l++];
+      if (l <= r) a += r - l + 1;
     }
     return a;
   }
 
-  public static void main(String[] args) {
+  /*
+   ---------------------------------------------------------------------------
+   B:
+    calculate the number of subarray with same left element
+    logic:
+      move right side when it is possible to move right side
+      else move left  if it is possible to move left
+      else stop and return result.
+  */
+  public int numSubarrayProductLessThanKSW2(int[] nums, int k) {
+    int l = 0, r = 0, p = nums[0], a = p < k ? 1 : 0, N = nums.length;
+    while (true) {
+      if (r + 1 < N && p * nums[r + 1] < k) {
+        r++;
+        p *= nums[r];
+        a++;
+      } else if (l + 1 < N) {
+        p /= nums[l];
+        l++;
+        if (p < k && l <= r) a += r - l + 1;
+      } else return a;
+    }
+  }
 
-    [10,5,2,6]
-    100
+  /*
+  -----------------------------------------------------------------------------
+  Binary search
+    convert A[i] to logA[i], else
+    premul -> presum, non-decreasing array
+    O(NlogN) time, O(N) space
+   */
+  public static int numSubarrayProductLessThanKBS(int[] nums, int K) {
+    if (K == 0) return 0;
+    double k = Math.log(K);
+    double[] p = new double[nums.length + 1]; // length+1
+    for (int i = 1; i < p.length; i++) p[i] = p[i - 1] + Math.log(nums[i - 1]);
 
+    int a = 0;
+    for (int i = 1; i < p.length; i++) {
+      int t = Arrays.binarySearch(p, i, p.length, p[i - 1] + k);
+      if (t < 0) t = ~t;
+      else while (i <= t && p[t] == p[t - 1]) t--;
+      t--;
+      if (i <= t) a += t - i + 1;
+    }
+    return a;
   }
 }
