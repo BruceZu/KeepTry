0b4fee6b96f69a2b449fa720c06f4b4c76d07b39
Bruce Zu <zu.bruce.china@gmail.com>
2021-10-13 23:31:07 -0700

    polish

--- arrows/src/main/java/array/Leetcode41FirstMissingPositive.java
+++ arrows/src/main/java/array/Leetcode41FirstMissingPositive.java
@@ -16,51 +16,78 @@
 package array;
 
 public class Leetcode41FirstMissingPositive {
+  /*
+      41. First Missing Positive
 
-  public int firstMissingPositive1(int[] nums) {
-    /*
-     Question:
-        n is nums.length
-        find the smallest number of the [1,...,n,n+1] but not in nums
-    Idea:
-        1. loop nums, for any nums[i] not in [1, n], reset its value as n+1
-        2. loop each nums[i] if it is in [1,n] then
-          res set nums[nums[i]-1] as negative if it not negative.
-        3. loop again to find the first positive nums[i]
-           i+1 is the answer.
-        O(N) time and O(1) space
-     */
-    /*
-    1 <= nums.length <= 300
-    -2^31 <= nums[i] <= 2^31 - 1
-    */
-    // TODO: checking nums is not null
+      Given an unsorted integer array nums, return the smallest missing positive integer.
+      You must implement an algorithm that runs in O(n) time and uses constant extra space.
 
-    int n = nums.length;
-    for (int i = 0; i < n; i++) {
-      if (nums[i] < 1 || nums[i] > n) nums[i] = n + 1;
+
+      Input: nums = [1,2,0]
+      Output: 3
+
+      Input: nums = [3,4,-1,1]
+      Output: 2
+
+
+      Input: nums = [7,8,9,11,12]
+      Output: 1
+
+      Input: nums = [1,2,2,2]
+      Output: 3
+
+      Constraints:
+      1 <= nums.length <= 5 * 10^5
+      -2^31 <= nums[i] <= 2^31 - 1
+
+   Usage
+        for each server name keep an allocated number array
+       allocate ServerName number
+          return the smaller available number, and added it to the array.
+       deallocate ServerName number
+         find and remove the number from the number
+  */
+  /*
+  Idea:
+     filter out not related number in [1,N]
+     the existence of v is marked by make the value in v's location: index v-1  as negative.
+     o no positive and negative, so use N+1 as no related number.
+     also note the duplicated elments
+  */
+  public int firstMissingPositive1(int[] A) {
+    int N = A.length;
+    for (int i = 0; i < N; i++) {
+      if (A[i] < 1 || A[i] > N) A[i] = N + 1;
     }
-    for (int i = 0; i < n; i++) {
-      int v = Math.abs(nums[i]); // Note here
-      if (v != n + 1 && nums[v - 1] > 0) nums[v - 1] = -nums[v - 1];
+    for (int i = 0; i < N; i++) {
+      int v = Math.abs(A[i]);
+      if (v != N + 1 && A[v - 1] > 0) A[v - 1] *= -1;
     }
-    for (int i = 0; i < n; i++) {
-      if (nums[i] > 0) return i + 1;
+    for (int i = 0; i < N; i++) {
+      if (A[i] > 0) return i + 1;
     }
-    // how about case [1,2,3]?
-    return n + 1;
+    return N + 1;
   }
 
-  // Alternative --------------------------------------------------------------
-  public int firstMissingPositive(int[] A) {
-    int n = A.length;
-    // O(N)
-    for (int i = 0; i < n; i++)
-      // At most N, in that case left in the outer loop need not go into the inner loop.
-      while (A[i] >=1  && A[i] <= n && A[A[i] - 1] != A[i]) swap(A, i, A[i] - 1);
+  /* --------------------------------------------------------------
+  O(N) time, O(1) extra space
+  the while loop:
+    at most N time
+    A[A[i] - 1] != A[i] avoid duplicated number
 
-    for (int i = 0; i < n; i++) if (A[i] != i + 1) return i + 1;
-    return n + 1;
+  */
+  public int firstMissingPositive(int[] A) {
+    int N = A.length;
+    for (int i = 0; i < N; i++) {
+      while (A[i] >= 1 && A[i] <= N && A[A[i] - 1] != A[i]) {
+        swap(A, i, A[i] - 1);
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      if (A[i] != i + 1) return i + 1;
+    }
+    return N + 1;
   }
 
   private void swap(int[] A, int l, int r) {
