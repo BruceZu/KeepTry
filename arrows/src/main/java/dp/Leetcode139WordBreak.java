586c746b0429816afeeac861362769554b54790a
Bruce Zu <zu.bruce.china@gmail.com>
2021-10-21 22:24:35 -0700

    Polish code of word break

--- arrows/src/main/java/dp/Leetcode139WordBreak.java
+++ arrows/src/main/java/dp/Leetcode139WordBreak.java
@@ -17,24 +17,38 @@ package dp;
 
 import java.util.List;
 
-/*
-Question
- Given a string s and a dictionary of strings wordDict,
- return true if s can be segmented into a space-separated
- sequence of one or more dictionary words.
- Note that the same word in the dictionary may be reused multiple
-      times in the segmentation.
+public class Leetcode139WordBreak {
+  /*
+    139. Word Break
 
+    Given a string s and a dictionary of strings wordDict, return true if
+    s can be segmented into a space-separated sequence of one or more dictionary words.
+
+    Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+    Input: s = "leetcode", wordDict = ["leet","code"]
+    Output: true
+    Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+    Input: s = "applepenapple", wordDict = ["apple","pen"]
+    Output: true
+    Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+    Note that you are allowed to reuse a dictionary word.
+
+
+    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    Output: false
+
+
+    Constraints:
 
     1 <= s.length <= 300
     1 <= wordDict.length <= 1000
     1 <= wordDict[i].length <= 20
     s and wordDict[i] consist of only lowercase English letters.
     All the strings of wordDict are unique.
-
- */
-public class Leetcode139WordBreak {
-
+  */
   /*
   O(M*L*N) time.
   N is the string length,
@@ -42,8 +56,9 @@ public class Leetcode139WordBreak {
   L: is max length of word in Directory
   O(N) space
   */
-  public boolean wordBreak2(String s, List<String> d) {
-    boolean[] dp = new boolean[s.length() + 1]; // dp[j]: status of sub string [0,j)
+  public boolean wordBreak_(String s, List<String> d) {
+    boolean[] dp =
+        new boolean[s.length() + 1]; // dp[j]: sub string [0,j-1] is possible to break into words
     dp[0] = true;
 
     for (int j = 1; j <= s.length(); j++) {
@@ -57,7 +72,40 @@ public class Leetcode139WordBreak {
     }
     return dp[s.length()];
   }
-  // -- with Trie -------------------------------------------------------------
+
+  /*---------------------------------------------------------------------------
+  Idea
+     convert directory to a Tire in O(M*L) time
+
+     dp[i]: true/false for index range [i, Length-1]
+            for calculating dp[s.length()-1], need dp[s.length()] which is by default is  true
+  O(max{L*N,M*L}) time.
+  N is the string length,
+  M: Directory word number
+  L: is max length of word in Directory
+  O(M*L) space
+  */
+  public boolean wordBreak(String s, List<String> d) {
+    T root = new T(), node;
+    for (String w : d) addWord(root, w); // O(M*L) time
+
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[s.length()] = true;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      // check if the substring, staring from index i,  starts with a word in directory.
+      T n = root;
+      for (int j = i; j < s.length() && n.nexts[(int) s.charAt(j)] != null; j++) {
+        n = n.nexts[(int) s.charAt(j)];
+        if (n.isEnd && dp[j + 1]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[0];
+  }
+
   public static class T {
     boolean isEnd;
     T[] nexts;
@@ -77,39 +125,5 @@ public class Leetcode139WordBreak {
       root = root.nexts[ci];
     }
     root.isEnd = true;
-  }
-  /*
-  Idea
-     convert directory to a Tire in O(M*L) time
-     dp[i]: true/false for index range [i, Length-1]
-     back forward loop string s, because words in Trie is left-right/top-down
-     for calculating dp[s.length()-1], need dp[s.length()] be true, which require
-     dp length is s.length() + 1.
-
-  O(max{L*N,M*L}) time.
-  N is the string length,
-  M: Directory word number
-  L: is max length of word in Directory
-  O(M*L) space
-  */
-  public boolean wordBreak(String s, List<String> d) {
-    // TODO check null
-    T root = new T(), node;
-    for (String w : d) addWord(root, w); // O(M*L) time
-
-    boolean[] dp = new boolean[s.length() + 1];
-    dp[s.length()] = true;
-
-    for (int i = s.length() - 1; i >= 0; i--) {
-      T n = root;
-      for (int j = i; j < s.length() && n.nexts[(int) s.charAt(j)] != null; j++) {
-        n = n.nexts[(int) s.charAt(j)];
-        if (n.isEnd && dp[j + 1]) {
-          dp[i] = true;
-          break;
-        }
-      }
-    }
-    return dp[0];
   }
 }
--- arrows/src/main/java/dp/Leetcode140WordBreakII.java
+++ arrows/src/main/java/dp/Leetcode140WordBreakII.java
@@ -18,69 +18,83 @@ package dp;
 import java.util.*;
 
 public class Leetcode140WordBreakII {
-  public List<String> wordBreak(String s, List<String> wordDict) {
-    /*
-    that the same word in the dictionary may be reused multiple times in the segmentation.
+  /*
+    140. Word Break II
+
+    Given a string s and a dictionary of strings wordDict,
+    add spaces in s to construct a sentence where each word
+    is a valid dictionary word.
+    Return all such possible sentences in any order.
+
+    Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+    Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+    Output: ["cats and dog","cat sand dog"]
+
+
+    Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+    Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+    Explanation: Note that you are allowed to reuse a dictionary word.
+
+
+    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    Output: []
+
+
+    Constraints:
 
     1 <= s.length <= 20
     1 <= wordDict.length <= 1000
     1 <= wordDict[i].length <= 10
     s and wordDict[i] consist of only lowercase English letters.
     All the strings of wordDict are unique.
-     */
-    // TODO: check corner caces
-    // O(?) time and space
+  */
 
-    // test cases:
-    // defend cases:
-    //    null,
-    //    duplicated value in dictionary
-    //    "亚洲"(industry code)
-    // special cases:
-    //    str does not contain any word in dictionary;
-    //     ""
-    //    str contain only one word not in dictionary;
+  /*
+  Idea:
+    DFS(backtracking, DP top-down which need a cache)
+    if current str is "" then return "";
+    For each word the current str starts with in dictionary:
+      merge it with all substring returned sentences(if existing) to result;
+      if no sub sentences, return empty result
+      if no word found the cur str starts with return empty result
 
-    /*
-    Idea:
-      DFS(backtracking, DP top-down which need a cache)
-      if current str is "" then return "";
-      For each word the current str starts with in dictionary:
-        merge it with all substring returned sentences(if existing) to result;
-        if no sub sentences, return empty result
-        if no word found the cur str starts with return empty result
+  It's a tree data structure
+  visit certain nodes multiple times, but visit each edge once and only once.
+  the number of edges depends on the construction of the input string and the word dictionary.
+  In the worst case, there could be NN valid postfixes, i.e. each prefix of the input
+  string is a valid word: s=aaa and the word dictionary as wordDict=["a", "aa", "aaa"].
+  N: the length of the input string
+  W: the number of words in the dictionary
 
-     TODO: O(?) time and space. depends on directory and str.
-    */
-
-    Set<String> d = new HashSet<>();
-    wordDict.stream().forEach(w -> d.add(w));
-    // assume no hash collision; or `d.addAll(wordDict);`
-    return dfs(s, d, new HashMap<>());
+   TODO: O(?) time and space.
+  */
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    return dfs(s, wordDict, new HashMap<>());
   }
 
-  List<String> dfs(String s, Set<String> d, Map<String, List<String>> c) {
+  /*
+   List with "": previous part of current sentence is valid and end.
+   empty list: current sentence is invalid
+  */
+  List<String> dfs(String s, List<String> d, Map<String, List<String>> cache) {
+    if (cache.containsKey(s)) return cache.get(s);
     // when to stop if dfs
-    if (c.containsKey(c)) return c.get(s);
     List<String> r = new LinkedList();
     if (s.isEmpty()) {
       r.add("");
-      // previous part of current sentence is valid and end. its meaning is not same as
-      // empty list which means
-      // can find word in dictionary the current s starts with and
-      // thus current sentence is invalid
       return r;
     }
-    for (String w : d) { // for each w from dictionary to check if s starts with it.
+    for (String w : d) { // skill for each w from dictionary to check if s starts with it.
       if (s.startsWith(w)) {
-        List<String> subs = dfs(s.substring(w.length()), d, c);
-        for (String stc : subs) {
-          String v = stc.isEmpty() ? "" : " " + stc;
+        List<String> list = dfs(s.substring(w.length()), d, cache);
+        for (String l : list) {
+          String v = l.isEmpty() ? "" : " " + l;
           r.add(w + v);
         }
       }
     }
-    c.put(s, r);
+    cache.put(s, r);
     return r; // empty list if no word the current s starts with
   }
 }
