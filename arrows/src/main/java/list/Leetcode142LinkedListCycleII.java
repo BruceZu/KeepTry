adc75bcdcf8579a8c16d3e7d727e48c1c0d46135
Bruce Zu <zu.bruce.china@gmail.com>
2022-01-04 22:34:10 -0800

    Floyd's Tortoise and Hare

--- arrows/src/main/java/list/Leetcode142LinkedListCycleII.java
+++ arrows/src/main/java/list/Leetcode142LinkedListCycleII.java
@@ -15,43 +15,70 @@
 
 package list;
 
-// Given a linked list, return the node where the cycle begins.
-// If there is no cycle, return null.
-// Note: Do not modify the linked list.
-// Follow up:
-// Can you solve it without using extra space?
-// https://leetcode.com/problems/linked-list-cycle-ii/
 public class Leetcode142LinkedListCycleII {
+  /*
+  Leetcode 142. Linked List Cycle II
 
-    // beats 20.26% of java submissions.
-    public static ListNode detectCycle(ListNode head) {
-        ListNode f = head;
-        ListNode l = head;
-        while (!(f == null || f.next == null)) {
-            f = f.next.next;
-            l = l.next;
-            if (f == l && f != null) {
-                while (head != f) {
-                    head = head.next;
-                    f = f.next;
-                }
-                return f;
-            }
-        }
-        return null;
-    }
+  Given the head of a linked list, return the node where the cycle begins.
+  If there is no cycle, return null.
 
-    // beats 20.26% of java submissions.
-    // it depends on there is no value of Integer.MIN_VALUE existing
-    public static ListNode detectCycle2(ListNode head) {
-        while (null != head) {
-            if (Integer.MIN_VALUE != head.val) {
-                head.val = Integer.MIN_VALUE;
-            } else {
-                return head;
-            }
-            head = head.next;
+  There is a cycle in a linked list if there is some node in the list that can be reached
+  again by continuously following the next pointer. Internally, pos is used to
+  denote the index of the node that tail's next pointer is connected to (0-indexed).
+  It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+  Do not modify the linked list.
+
+
+  Input: head = [3,2,0,-4], pos = 1
+  Output: tail connects to node index 1
+  Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+
+  Input: head = [1,2], pos = 0
+  Output: tail connects to node index 0
+  Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+
+  Input: head = [1], pos = -1
+  Output: no cycle
+  Explanation: There is no cycle in the linked list.
+
+
+  Constraints:
+
+  The number of the nodes in the list is in the range [0, 104].
+  -105 <= Node.val <= 105
+  pos is -1 or a valid index in the linked-list.
+
+  Follow up: Can you solve it using O(1) (i.e. constant) memory?
+   */
+  /*
+  Floyd's Tortoise and Hare
+
+  Floyd's algorithm is separated into two distinct phases.
+   - it determines whether a cycle is present in the list.
+     If no cycle is present, it returns null immediately
+  -  Otherwise, it uses the located "intersection node" to
+     find the entrance to the cycle.
+
+   */
+  public static ListNode detectCycle(ListNode head) {
+    ListNode h = head; // hare
+    ListNode t = head; // tortoise
+    while (!(h == null || h.next == null)) {
+      h = h.next.next;
+      t = t.next;
+      if (h == t && h != null) {
+        while (head != t) {
+          head = head.next;
+          t = t.next;
         }
-        return null;
+        return head;
+      }
     }
+    return null;
+  }
 }
