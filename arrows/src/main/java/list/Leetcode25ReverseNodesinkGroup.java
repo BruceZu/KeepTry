a4371e4c4c53a3e372b4111e9debdc561054b012
Bruce Zu <zu.bruce.china@gmail.com>
2022-01-18 00:01:30 -0800

    Polish Reverse Nodes in k Group

--- arrows/src/main/java/list/Leetcode25ReverseNodesinkGroup.java
+++ arrows/src/main/java/list/Leetcode25ReverseNodesinkGroup.java
@@ -17,155 +17,154 @@ package list;
 
 public class Leetcode25ReverseNodesinkGroup {
   /*
-     25. Reverse Nodes in k-Group
+       25. Reverse Nodes in k-Group
 
-      a linked list, reverse the nodes of a linked list k at a time and return its modified list.
-      k is a positive integer and is less than or equal to the length of the linked list.
-      If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+        a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+        k is a positive integer and is less than or equal to the length of the linked list.
+        If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
 
-      You may not alter the values in the list's nodes, only nodes themselves may be changed.
-
-
-   Input: head = [1,2,3,4,5], k = 2
-   Output: [2,1,4,3,5]
+        You may not alter the values in the list's nodes, only nodes themselves may be changed.
 
 
-   Input: head = [1,2,3,4,5], k = 3
-   Output: [3,2,1,4,5]
+     Input: head = [1,2,3,4,5], k = 2
+     Output: [2,1,4,3,5]
 
 
-   Input: head = [1,2,3,4,5], k = 1
-   Output: [1,2,3,4,5]
+     Input: head = [1,2,3,4,5], k = 3
+     Output: [3,2,1,4,5]
 
 
-   Input: head = [1], k = 1
-   Output: [1]
+     Input: head = [1,2,3,4,5], k = 1
+     Output: [1,2,3,4,5]
+
+
+     Input: head = [1], k = 1
+     Output: [1]
 
 
 
-   Constraints:
+  Constraints:
 
-       The number of nodes in the list is in the range sz.
-       1 <= sz <= 5000
-       0 <= Node.val <= 1000
-       1 <= k <= sz
+  The number of nodes in the list is n.
+  1 <= k <= n <= 5000
+  0 <= Node.val <= 1000
 
 
-   Follow-up: Can you solve the problem in O(1) extra memory space?
-  */
+     Follow-up: Can you solve the problem in O(1) extra memory space?
+    */
 
   /* --------------------------------------------------------------------------
-  Idea: boundary node relation-recursion
-    watch the boundary node relation between different groups
-    group inner is as reverse one by one
+  recursion
+    list 1,2,3,4,5
+    reverse two nodes at a time.
+    reverse the first two nodes thus getting 2,1.
+    assume the following will be 4,3,5.
+    Now, need hookup 1-> 4, let 4 is the pre
 
 
-  before and after 1  reverse operation
-   pre(null)       cur  -->  ?
-   null       <--  pre      (cur,next)
-
-  O(N) time.
-  O(N/k) spaced, used by stack.
-   */
-  public ListNode reverseKGroup__(ListNode head, int k) {
-    if (head == null) return null;
-    ListNode kNode = head;
+   O(N) time.
+   O(N/k) spaced, used by stack.
+    */
+  public ListNode reverseKGroup__(ListNode h, int size) {
+    if (h == null) return h; // this is possible
+    ListNode tail = h; // group head h and tail
     int i = 1;
-    while (kNode.next != null && i + 1 <= k) {
+    while (i < size && tail.next != null) {
       i++;
-      kNode = kNode.next;
+      tail = tail.next;
     }
-    if (i < k) return head;
-    ListNode pre = reverseKGroup(kNode.next, k), cur = head;
-    while (pre != kNode) {
+    if (i < size) return h;
+    else {
+      ListNode pre = reverseKGroup__(tail.next, size), cur = h;
+      while (pre != tail) {
+        ListNode next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+      }
+      return pre;
+    }
+  }
+
+  /* --------------------------------------------------------------------------
+  Watch:  https://imgur.com/a/0i4d9m8
+  before and after reverse group  https://imgur.com/a/DivXjba
+
+  keep
+     - `preTail` node: keep the last group node used to connect current reversed group head node( `pre` )
+     - `resultHead` node: keep the result list head node
+     group head h and tail:
+     - `tail` at the group end node or last not null node
+     - input parameter `h` always is the head of group first node
+   They can be global variables
+
+   local variable:  `pr`e=null, `cur`=`h`, `next` are local variables used to reverse current group.
+  O(N) time.
+  O(1) space
+  */
+  public ListNode reverseKGroup_(ListNode h, int size) {
+    ListNode resultHead = null, preTail = null, tail = h;
+    // while (true) will be in endless loop  e.g. size is 1 and from cur and h, tail is null
+    while (h != null) {
+      int i = 1;
+      while (i < size && tail.next != null) {
+        i++;
+        tail = tail.next;
+      }
+      // tail always is not null
+      if (i != size) {
+        preTail.next = h; // need not: if(preTail !=null), as 1 <= k <= n <= 5000
+        break;
+      }
+      ListNode pre = null, cur = h;
+      // if tail.next is null, then while(cur!=tail.next)  always true;
+      // instead use  while (pre != tail) or use i-->0
+      while (pre != tail) {
+        ListNode next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+      }
+      if (resultHead == null) resultHead = pre;
+      if (preTail != null) preTail.next = pre;
+      preTail = h;
+      // do not use h = tail.next because tail has been reversed
+      h = cur;
+      tail = cur;
+    }
+    return resultHead;
+  }
+  /* --------------------------------------------------------------------------
+   abstract out the group reverse in a method
+  */
+  public ListNode reverseKGroup(ListNode h, int size) {
+    ListNode preTail = null, resultHead = null, tail = h;
+    int i = 1;
+    while (tail != null) {
+      if (i % size == 0) {
+        if (resultHead == null) resultHead = tail;
+        if (preTail != null) preTail.next = tail; // it is tail not h
+        preTail = h;
+
+        h = reverseGroupPointer(h, tail);
+        tail = h;
+      } else {
+        tail = tail.next;
+      }
+      i++;
+    }
+    preTail.next = h; // left part
+    return resultHead;
+  }
+  // see https://imgur.com/3lTkqRW   return next group head
+  private ListNode reverseGroupPointer(ListNode h, ListNode tail) {
+    ListNode pre = null, cur = h;
+    while (pre != tail) {
       ListNode next = cur.next;
       cur.next = pre;
       pre = cur;
       cur = next;
     }
-    return pre;
-  }
-
-  /* --------------------------------------------------------------------------
-  Idea: boundary node relation-iterator
-    returnNode need assign value only once.
-
-    preTail->   cur(tail) ....kNode
-
-    the cur/head will be tail after reverse operation
-    after k time reverse operation, pre is at the kNode
-
-    in case the left nodes number < k, need break the loop
-
-  O(N) time.
-  O(1)
-   */
-  public ListNode reverseKGroup(ListNode head, int k) {
-    ListNode r = null, preTail = null, cur = head, tail = cur;
-    while (cur != null) {
-      // kNode
-      int i = 1;
-      ListNode kNode = cur;
-      while (i + 1 <= k && kNode.next != null) {
-        kNode = kNode.next;
-        i++;
-      }
-      // less than k, Node break here
-      if (i < k) {
-        if (preTail != null) preTail.next = cur;
-        return r;
-      }
-
-      ListNode pre = null;
-      while (i > 0) {
-        ListNode next = cur.next;
-        cur.next = pre;
-        pre = cur;
-        cur = next;
-        i--;
-      }
-
-      if (r == null) r = pre;
-      if (preTail != null) preTail.next = pre;
-      preTail = tail;
-      tail = cur;
-    }
-    return r;
-  }
-  /* --------------------------------------------------------------------------
-  Idea: boundary node relation-iterator
-  use a dummy node ahead
-  dummy node
-   - initially it points to the head of lest nodes, when left nodes number <k, need not any further action.
-   - during reverse operation it points to the node next reversed point will point to
-  */
-  public ListNode reverseKGroup3(ListNode head, int k) {
-    ListNode dummy = new ListNode(-1);
-    dummy.next = head;
-
-    ListNode preTail = dummy, cur = head;
-
-    int i = 1;
-    while (cur != null) {
-      if (i % k == 0) {
-        preTail = reverse(preTail, cur.next);
-        cur = preTail.next;
-      }
-      else cur = cur.next;
-      i++;
-    }
-    return dummy.next;
-  }
-
-  private static ListNode reverse(ListNode preTail, ListNode nextCur) {
-    ListNode tail = preTail.next, cur = tail.next, next;
-    while (cur != nextCur) {
-      next = cur.next;
-      cur.next = preTail.next;
-      preTail.next = cur;
-      cur = next;
-    }
-    tail.next = nextCur;
-    return tail;
+    return cur;
   }
 }
