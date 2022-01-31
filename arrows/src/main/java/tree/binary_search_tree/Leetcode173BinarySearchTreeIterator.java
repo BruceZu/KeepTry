2d62c9976f9ad67cd2beab7ba39bcbb2a61aeb3e
Bruce Zu <zu.bruce.china@gmail.com>
2022-01-30 17:53:21 -0800

    Polish Leetcode173BinarySearchTreeIterator

--- arrows/src/main/java/tree/binary_search_tree/Leetcode173BinarySearchTreeIterator.java
+++ arrows/src/main/java/tree/binary_search_tree/Leetcode173BinarySearchTreeIterator.java
@@ -17,15 +17,59 @@ package tree.binary_search_tree;
 
 import java.util.Stack;
 
-/**
- * <pre>
- * <a href="https://leetcode.com/problems/binary-search-tree-iterator/">LeetCode</a>
- * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
- *
- * Calling next() will return the next smallest number in the BST.
- *
- * Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
- * where h is the height of the tree.
+/*
+Leetcode 173. Binary Search Tree Iterator
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class.
+The root of the BST is given as part of the constructor.
+The pointer should be initialized to a non-existent number smaller than any element in the BST.
+
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer,
+                 otherwise returns false.
+
+int next() Moves the pointer to the right, then returns the number at the pointer.
+
+Notice that by initializing the pointer to a non-existent smallest number,
+the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid.
+That is, there will be at least a next number in the in-order traversal when next() is called.
+
+
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+0 <= Node.val <= 106
+At most 105 calls will be made to hasNext, and next.
+
+
+Follow up:
+
+ Could you implement next() and hasNext()
+ to run in average O(1) time and use O(h) memory,
+ where h is the height of the tree?
  */
 public class Leetcode173BinarySearchTreeIterator {
   /** Definition for binary tree */
@@ -39,11 +83,19 @@ public class Leetcode173BinarySearchTreeIterator {
     }
   }
 
-  // implement ----------------------------------------------------------------
+  /*  -------------------------------------------------------------------------
+   Watch:
+     Only one direction operation, never step back forward.
+
+  space complexity is O(H)
+  average time O(1)  for BSTIterator(), hasNext(), next();
+  */
   class BSTIterator {
-    // top down in ascending order. not descending
+    // keek the peak of stack as the next()
+    // top -> down is in ascending order, keep only right side parents.
     private Stack<TreeNode> s = new Stack();
 
+    // O(N) in the worse case
     public BSTIterator(TreeNode n) {
       while (n != null) {
         s.push(n);
@@ -55,6 +107,7 @@ public class Leetcode173BinarySearchTreeIterator {
       return !s.empty();
     }
 
+    // the amortized (average) time complexity: O(1). O(H) in the worst case.
     public int next() {
       if (!hasNext()) return -1;
 
