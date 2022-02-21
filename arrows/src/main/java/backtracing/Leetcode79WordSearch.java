b9cf6847f2cb94d4b33ea85dcac686cc9d751810
Bruce Zu <zu.bruce.china@gmail.com>
2022-02-20 16:37:09 -0800

    Polish Leetcode 79

--- arrows/src/main/java/backtracing/Leetcode79WordSearch.java
+++ arrows/src/main/java/backtracing/Leetcode79WordSearch.java
@@ -20,37 +20,37 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-/*
-79. Word Search
-
-Given an m x n grid of characters board and a string word, return true if word exists in the grid.
-
-The word can be constructed from letters of sequentially adjacent cells,
-where adjacent cells are horizontally or vertically neighboring.
-The same letter cell may not be used more than once.
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
-Output: true
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
-Output: true
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
-Output: false
-
-
-Constraints:
-
-m == board.length
-n = board[i].length
-1 <= m, n <= 6
-1 <= word.length <= 15
-board and word consists of only lowercase and uppercase English letters.
-
-
-Follow up: Could you use search pruning to make your solution faster with a larger board?
-*/
 public class Leetcode79WordSearch {
+  /*
+  79. Word Search
+
+  Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+  The word can be constructed from letters of sequentially adjacent cells,
+  where adjacent cells are horizontally or vertically neighboring.
+  The same letter cell may not be used more than once.
+
+  Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+  Output: true
+
+  Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+  Output: true
+
+  Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+  Output: false
+
+
+  Constraints:
+
+  m == board.length
+  n = board[i].length
+  1 <= m, n <= 6
+  1 <= word.length <= 15
+  board and word consists of only lowercase and uppercase English letters.
+
+
+  Follow up: Could you use search pruning to make your solution faster with a larger board?
+  */
 
   static int M, N;
   static char[][] grid;
