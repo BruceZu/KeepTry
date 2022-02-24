bceb9f08411a2a05b38cdd6b548b3bef776448d4
Bruce Zu <zu.bruce.china@gmail.com>
2022-02-24 11:20:24 -0800

    Polish Leetcode 398. Random Pick Index

--- arrows/src/main/java/random/Leetcode398RandomPickIndex.java
+++ arrows/src/main/java/random/Leetcode398RandomPickIndex.java
@@ -19,10 +19,36 @@ import java.util.Random;
 
 public class Leetcode398RandomPickIndex {
   /*
-  1 <= nums.length <= 2 * 104
-  -231 <= nums[i] <= 231 - 1
-  target is an integer from nums.
-  At most 104 calls will be made to pick.
+      Leetcode 398. Random Pick Index
+      Given an integer array nums with possible duplicates,
+      randomly output the index of a given target number.
+      You can assume that the given target number must exist in the array.
+
+    Implement the Solution class:
+
+    Solution(int[] nums) Initializes the object with the array nums.
+    int pick(int target) Picks a random index i from nums where nums[i] == target.
+    If there are multiple valid i's, then each index should have an equal probability of returning.
+
+
+    Input
+    ["Solution", "pick", "pick", "pick"]
+    [[[1, 2, 3, 3, 3]], [3], [1], [3]]
+    Output
+    [null, 4, 0, 2]
+
+    Explanation
+    Solution solution = new Solution([1, 2, 3, 3, 3]);
+    solution.pick(3); // It should return either index 2, 3, or 4 randomly.
+                         Each index should have equal probability of returning.
+    solution.pick(1); // It should return 0. Since in the array only nums[0] is equal to 1.
+    solution.pick(3); // It should return either index 2, 3, or 4 randomly.
+                         Each index should have equal probability of returning.
+
+      1 <= nums.length <= 2 * 104
+      -2^31 <= nums[i] <= 2^31 - 1
+      target is an integer from nums.
+      At most 10^4 calls will be made to pick.
   */
   static class Solution {
     int[] nums;
@@ -40,11 +66,11 @@ public class Leetcode398RandomPickIndex {
      */
     // O(N) time and O(1) space
     public int pick(int target) {
-      int counts = 0;
+      int cnt = 0;
       int resultIndex = 0;
       for (int i = 0; i < nums.length; i++) {
         if (nums[i] != target) continue;
-        if (r.nextInt(++counts) == 0) resultIndex = i;
+        if (r.nextInt(++cnt) == 0) resultIndex = i;
       }
       return resultIndex;
     }
