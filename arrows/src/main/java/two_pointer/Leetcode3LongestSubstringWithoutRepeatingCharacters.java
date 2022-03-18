05c8856fd93930ea613c7d99d16a4c87fb1ea689
Bruce Zu <zu.bruce.china@gmail.com>
2022-03-17 23:41:04 -0700

    Polish Leetcode3LongestSubstringWithoutRepeatingCharacters

--- arrows/src/main/java/two_pointer/Leetcode3LongestSubstringWithoutRepeatingCharacters.java
+++ arrows/src/main/java/two_pointer/Leetcode3LongestSubstringWithoutRepeatingCharacters.java
@@ -60,27 +60,72 @@ public class Leetcode3LongestSubstringWithoutRepeatingCharacters {
   O(N) time,
   O(Min{N,m}) space, map is upper bounded by the size of the string n and the size of the charset/alphabet m.
   */
-  public int lengthOfLongestSubstring_(String str) {
+  public int lengthOfLongestSubstring__(String str) {
     if (str == null || str.length() == 0) return 0;
     char[] arr = str.toCharArray();
     Map<Character, Integer> map = new HashMap<>();
     int l = 0, r = 0, a = 0;
     for (; r < arr.length; r++) {
-      char cur = arr[r];
-      if (!map.containsKey(cur)) {
-        map.put(cur, 1);
+      char c = arr[r];
+      if (!map.containsKey(c)) {
+        map.put(c, 1);
         a = Math.max(a, r - l + 1);
       } else {
-        while (map.containsKey(cur)) {
+        while (map.containsKey(c)) {
           char lv = arr[l];
           map.put(lv, map.get(lv) - 1);
           if (map.get(lv) == 0) map.remove(lv);
           l++;
         }
-        map.put(cur, 1);
+        map.put(c, 1);
       }
     }
     return a;
+  }
+  /*
+  Actually this is my version
+  each time move one step of right pointer and maintain
+    - distinguish sliding window
+    - current longest sub distinguish string
+
+  The original question need return the longest substring itself
+   */
+  public int lengthOfLongestSubstring(String str) {
+    if (str == null || str.length() == 0) return 0;
+    char[] s = str.toCharArray();
+    int N = s.length;
+    Map<Character, Integer> map = new HashMap<>();
+    String ans = "";
+    int len = 0;
+    int l = 0, r = 0;
+    // r is the right pointer, related char will be accepted in sliding window specified by index
+    // range [l,r]
+    while (true) {
+      if (r < N) {
+        char c = s[r];
+        map.putIfAbsent(c, 0);
+        if (map.get(c) == 1) { // duplicated char
+          map.put(c, 2);
+          while (map.get(s[r]) == 2 && l <= r) {
+            map.put(s[l], map.get(s[l]) - 1); // forget updating the window char frequency
+            if (map.get(s[l]) == 0) map.remove(s[l]);
+            l++;
+          }
+          r++;
+        } else { // new distinguish char
+          map.put(c, 1);
+          r++;
+          if (r - l > len) {
+            len = r - l;
+            ans = str.substring(l, r);
+          }
+        }
+      } else { // r reaches the end of string
+        break;
+      }
+    }
+
+    return len;
   }
 
   /*
@@ -99,18 +144,18 @@ public class Leetcode3LongestSubstringWithoutRepeatingCharacters {
    O(N) time,
    O(m) space, at most 128
   */
-  public int lengthOfLongestSubstring(String s) {
+  public int lengthOfLongestSubstring_(String s) {
     if (s == null || s.length() == 0) return 0;
     Map<Character, Integer> map = new HashMap<>(); // char -> current index
     char[] a = s.toCharArray();
     int l = 0, r = 0, max = 0;
     for (; r < a.length; r++) {
-      char cur = a[r];
-      if (map.containsKey(cur)) {
+      char c = a[r];
+      if (map.containsKey(c)) {
         max = Math.max(max, r - l);
-        l = Math.max(map.get(cur) + 1, l); // at least move to index of map.get(cur) + 1
+        l = Math.max(map.get(c) + 1, l); // at least move to index of map.get(cur) + 1
       }
-      map.put(cur, r);
+      map.put(c, r);
     }
     max = Math.max(max, r - l);
     return max;
